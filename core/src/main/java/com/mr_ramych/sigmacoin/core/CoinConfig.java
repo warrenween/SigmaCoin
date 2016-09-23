e30b396c8f9fae1d74369050ffc31debd51874bc
Mr_Ramych <qwertyppp11@gmail.com>
2016-09-23 21:02:36 +0300

    changed project structure

---
+++ core/src/main/java/com/mr_ramych/sigmacoin/core/CoinConfig.java
@@ -0,0 +1,8 @@
+package com.mr_ramych.sigmacoin.core;
+
+public class CoinConfig {
+
+    public static String COIN_NAME = "Sigmacoin";
+    public static String COIN_SHORT_NAME = "SGC";
+
+}
--- src/main/java/com/mr_ramych/sigmacoin/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mr_ramych.sigmacoin;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
