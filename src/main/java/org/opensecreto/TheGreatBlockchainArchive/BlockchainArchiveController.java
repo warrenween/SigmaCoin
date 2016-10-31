e9d35424faaac13fbc21b583708f58e4e5f0a735
Mr_Ramych <qwertyppp11@gmail.com>
2016-10-31 21:16:38 +0300

    imroved code and wrote put method

--- src/main/java/org/opensecreto/TheGreatBlockchainArchive/BlockchainArchiveController.java
+++ src/main/java/org/opensecreto/TheGreatBlockchainArchive/BlockchainArchiveController.java
@@ -29,29 +29,68 @@ public class BlockchainArchiveController {
     }
 
     public String get(byte[] hash) throws IOException {
+        BlockIndex index = findHash(hash);
+        if (index == null) {
+            return null;
+        } else {
+            byte[] data = new byte[index.length];
+            blockchain.readFully(data, index.offset, index.length);
+            return new String(data);
+        }
+    }
+
+    /**
+     * @param hash String representing hex hash
+     * @param data
+     */
+    public void put(String hash, String data) throws IOException {
+        this.put(DatatypeConverter.parseHexBinary(hash), data);
+    }
+
+    public void put(byte[] hash, String data) throws IOException {
+        if (hash.length != configuration.getHashLength()) {
+            throw new IllegalArgumentException("Given hash length " + hash.length + " " +
+                    "is not equal to configuration length " + configuration.getHashLength() + ".");
+        }
+
+        blockchain.seek(blockchain.length() - 1);
+        int index = (int) blockchain.getFilePointer();
+        blockchain.write(data.getBytes());
+
+        this.index.seek(configuration.getHashLength() - 1);
+        this.index.write(hash);
+        this.index.write(index);
+        this.index.write(data.getBytes().length);
+    }
+
+    private BlockIndex findHash(byte[] hash) throws IOException {
         index.seek(0);
         byte[] tempHash = new byte[configuration.getHashLength()];
         long line;
         while (index.getFilePointer() < index.length()) {
             index.read(tempHash);
-            long offset = index.readLong();
-            long length = index.readLong();
+            int offset = index.readInt();
+            int length = index.readInt();
 
-            if (Arrays.equals(hash, hash)) {
+            if (Arrays.equals(tempHash, hash)) {
                 byte[] bytes = new byte[(int) length];
                 blockchain.readFully(bytes, (int) offset, (int) length);
-                return new String(bytes);
+                return new BlockIndex(hash, offset, length);
             }
         }
         return null;
     }
 
-    /**
-     * @param key  String representing hex hash
-     * @param data
-     */
-    public void put(String key, String data) {
+    private static class BlockIndex {
+        public final byte[] hash;
+        public final int offset;
+        public final int length;
 
+        public BlockIndex(byte[] hash, int offset, int length) {
+            this.hash = hash;
+            this.offset = offset;
+            this.length = length;
+        }
     }
 
 }
