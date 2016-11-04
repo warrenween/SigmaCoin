c68cf629ca63bb61bf5c2e7a4e2827a3f51e70a8
Mr_Ramych <qwertyppp11@gmail.com>
2016-11-04 15:12:58 +0300

    ContextElementType normalizer

--- src/main/java/org/opensecreto/sigmascipt/context/item/ContextElementType.java
+++ src/main/java/org/opensecreto/sigmascipt/context/item/ContextElementType.java
@@ -1,5 +1,62 @@
 package org.opensecreto.sigmascipt.context.item;
 
+import org.opensecreto.sigmascipt.exceptions.IllegalOperationException;
+
 public enum ContextElementType {
     OBJECT, BYTE, SHORT, INTEGER, LONG, FLOAT, DOUBLE;
+
+    public static ContextElementType normalize(ContextElementType type1, ContextElementType type2)
+            throws IllegalOperationException {
+        if ((type1 == ContextElementType.OBJECT)
+                || (type2 == ContextElementType.OBJECT)) {
+            throw new IllegalOperationException("Can not normalize objects.");
+        }
+        return fromOrd(Math.max(toOrd(type1), toOrd(type2)));
+    }
+
+    private static int toOrd(ContextElementType type) {
+        switch (type) {
+            case OBJECT:
+                return -1;
+            case BYTE:
+                return 0;
+            case SHORT:
+                return 1;
+            case INTEGER:
+                return 2;
+            case LONG:
+                return 3;
+            case FLOAT:
+                return 4;
+            case DOUBLE:
+                return 5;
+            default:
+                if (type == null) {
+                    throw new NullPointerException("type is null");
+                }
+                throw new RuntimeException("Unexpected type: " + type.toString());
+        }
+    }
+
+    private static ContextElementType fromOrd(int ord) {
+        switch (ord) {
+            case -1:
+                return OBJECT;
+            case 0:
+                return BYTE;
+            case 1:
+                return SHORT;
+            case 2:
+                return INTEGER;
+            case 3:
+                return LONG;
+            case 4:
+                return FLOAT;
+            case 5:
+                return DOUBLE;
+            default:
+                throw new IllegalArgumentException("Illegal ord: " + ord + ". " + "" +
+                        "Expected from -1 (inclusive) to 5(inclusive).");
+        }
+    }
 }
---
+++ src/test/java/org/opensecreto/sigmascript/context/item/ContextElementTypeTest.java
@@ -0,0 +1,68 @@
+package org.opensecreto.sigmascript.context.item;
+
+import org.assertj.core.api.Assertions;
+import org.opensecreto.sigmascipt.exceptions.IllegalOperationException;
+import org.testng.annotations.Test;
+
+import static org.opensecreto.sigmascipt.context.item.ContextElementType.*;
+
+public class ContextElementTypeTest {
+
+    @Test
+    public void testNormalization() throws IllegalOperationException {
+        Assertions.assertThatThrownBy(() ->
+                normalize(OBJECT, BYTE)
+        ).isInstanceOf(IllegalOperationException.class);
+
+        Assertions.assertThatThrownBy(() ->
+                normalize(BYTE, OBJECT)
+        ).isInstanceOf(IllegalOperationException.class);
+
+        Assertions.assertThatThrownBy(() ->
+                normalize(OBJECT, OBJECT)
+        ).isInstanceOf(IllegalOperationException.class);
+
+        Assertions.assertThat(normalize(BYTE, BYTE)).isEqualTo(BYTE);
+        Assertions.assertThat(normalize(BYTE, SHORT)).isEqualTo(SHORT);
+        Assertions.assertThat(normalize(BYTE, INTEGER)).isEqualTo(INTEGER);
+        Assertions.assertThat(normalize(BYTE, LONG)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(BYTE, FLOAT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(BYTE, DOUBLE)).isEqualTo(DOUBLE);
+
+        Assertions.assertThat(normalize(SHORT, BYTE)).isEqualTo(SHORT);
+        Assertions.assertThat(normalize(SHORT, SHORT)).isEqualTo(SHORT);
+        Assertions.assertThat(normalize(SHORT, INTEGER)).isEqualTo(INTEGER);
+        Assertions.assertThat(normalize(SHORT, LONG)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(SHORT, FLOAT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(SHORT, DOUBLE)).isEqualTo(DOUBLE);
+
+        Assertions.assertThat(normalize(INTEGER, BYTE)).isEqualTo(INTEGER);
+        Assertions.assertThat(normalize(INTEGER, SHORT)).isEqualTo(INTEGER);
+        Assertions.assertThat(normalize(INTEGER, INTEGER)).isEqualTo(INTEGER);
+        Assertions.assertThat(normalize(INTEGER, LONG)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(INTEGER, FLOAT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(INTEGER, DOUBLE)).isEqualTo(DOUBLE);
+
+        Assertions.assertThat(normalize(LONG, BYTE)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(LONG, SHORT)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(LONG, INTEGER)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(LONG, LONG)).isEqualTo(LONG);
+        Assertions.assertThat(normalize(LONG, FLOAT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(LONG, DOUBLE)).isEqualTo(DOUBLE);
+
+        Assertions.assertThat(normalize(FLOAT, BYTE)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(FLOAT, SHORT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(FLOAT, INTEGER)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(FLOAT, LONG)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(FLOAT, FLOAT)).isEqualTo(FLOAT);
+        Assertions.assertThat(normalize(FLOAT, DOUBLE)).isEqualTo(DOUBLE);
+
+        Assertions.assertThat(normalize(DOUBLE, BYTE)).isEqualTo(DOUBLE);
+        Assertions.assertThat(normalize(DOUBLE, SHORT)).isEqualTo(DOUBLE);
+        Assertions.assertThat(normalize(DOUBLE, INTEGER)).isEqualTo(DOUBLE);
+        Assertions.assertThat(normalize(DOUBLE, LONG)).isEqualTo(DOUBLE);
+        Assertions.assertThat(normalize(DOUBLE, FLOAT)).isEqualTo(DOUBLE);
+        Assertions.assertThat(normalize(DOUBLE, DOUBLE)).isEqualTo(DOUBLE);
+    }
+
+}
