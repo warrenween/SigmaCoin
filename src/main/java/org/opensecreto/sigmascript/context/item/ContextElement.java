d004f5f6da2d3ad403ca46753f284c30bb595ed3
Mr_Ramych <qwertyppp11@gmail.com>
2016-11-04 16:22:23 +0300

    get element by type

--- src/main/java/org/opensecreto/sigmascript/context/item/ContextElement.java
+++ src/main/java/org/opensecreto/sigmascript/context/item/ContextElement.java
@@ -2,6 +2,30 @@ package org.opensecreto.sigmascript.context.item;
 
 public interface ContextElement {
 
+    public static ContextElement getElementByType(ContextElementType type) {
+        switch (type) {
+            // TODO: 04.11.2016 add getting object
+            case BYTE:
+                return new ByteContextElement();
+            case SHORT:
+                return new ShortContextElement();
+            case INTEGER:
+                return new IntegerContextElement();
+            case LONG:
+                return new LongContextElement();
+            case FLOAT:
+                return new FloatContextElement();
+            case DOUBLE:
+                return new DoubleContextElement();
+            case BOOLEAN:
+                return new BooleanContextElement();
+            default:
+                if (type == null) {
+                    throw new NullPointerException("Type is null");
+                }
+                throw new RuntimeException("Unexpected type: " + type.toString());
+        }
+    }
     public ContextElementType getType();
 
 }
