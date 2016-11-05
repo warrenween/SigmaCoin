8649984a679228c6c33a8463f275192c7fbea7bb
Mr_Ramych <qwertyppp11@gmail.com>
2016-11-05 22:28:31 +0300

    script controller

---
+++ src/main/java/org/opensecreto/sigmascript/ScriptController.java
@@ -0,0 +1,15 @@
+package org.opensecreto.sigmascript;
+
+import org.opensecreto.sigmascript.context.Context;
+
+import java.util.HashMap;
+
+public class ScriptController {
+
+    private HashMap<String, Context> namespace = new HashMap<>(16);
+
+    public void addContext(String namespace, Context context) {
+        
+    }
+
+}
