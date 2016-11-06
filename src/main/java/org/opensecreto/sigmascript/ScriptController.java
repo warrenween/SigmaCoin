33b3ad353ec709d5fa4791c4c0ada3ef7eb82648
Mr_Ramych <qwertyppp11@gmail.com>
2016-11-06 21:31:33 +0300

    removed package system

--- src/main/java/org/opensecreto/sigmascript/ScriptController.java
+++ src/main/java/org/opensecreto/sigmascript/ScriptController.java
@@ -1,12 +1,6 @@
 package org.opensecreto.sigmascript;
 
-import org.opensecreto.sigmascript.context.Package;
-
-import java.util.HashMap;
-
 public class ScriptController {
-
-    private HashMap<String, Package> globalPackages = new HashMap<>();
 
     public void parseScript(String data) {
 
