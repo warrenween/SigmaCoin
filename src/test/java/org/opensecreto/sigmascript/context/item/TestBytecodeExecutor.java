c839569b857c060a78afd076abf6b4a3e395abc2
Mr_Ramych <qwertyppp11@gmail.com>
2017-01-22 20:57:56 +0300

    bit testing

--- src/test/java/org/opensecreto/sigmascript/context/item/TestBytecodeExecutor.java
+++ src/test/java/org/opensecreto/sigmascript/context/item/TestBytecodeExecutor.java
@@ -8,8 +8,7 @@ import org.testng.annotations.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.opensecreto.sigmascript.Opcodes.OP_JUMP_M;
-import static org.opensecreto.sigmascript.Opcodes.OP_PUSH;
+import static org.opensecreto.sigmascript.Opcodes.*;
 
 @Test
 public class TestBytecodeExecutor {
@@ -41,8 +40,20 @@ public class TestBytecodeExecutor {
         executor.storageBreakpoints.add(2L);
         executor.execute();
 
-        assertThat(executor.getPointer()).isEqualTo(0);
+        assertThat(executor.getPointer()).isEqualTo(0L);
         assertThat(executor.getMode()).isTrue();
+    }
+
+    public void testReturn() {
+        StorageManager storage = mock(StorageManager.class);
+        when(storage.getByte(0)).thenReturn(OP_PUSH);
+        when(storage.getByte(1)).thenReturn((byte) 27);
+        when(storage.getByte(2)).thenReturn(OP_RETURN);
+        executor.setStorage(storage);
+
+        executor.execute();
+
+        assertThat(executor.getExitCode()).isEqualTo((byte) 27);
     }
 
 }
