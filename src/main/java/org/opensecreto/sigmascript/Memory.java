b67d0504a2dcd228f7e477f0d6a0d51d605c09c5
Mr_Ramych <qwertyppp11@gmail.com>
2017-01-27 19:25:08 +0300

    Memory class

---
+++ src/main/java/org/opensecreto/sigmascript/Memory.java
@@ -0,0 +1,48 @@
+package org.opensecreto.sigmascript;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Memory {
+
+    private final Map<Long, Byte> memory = new HashMap<>();
+
+    public static Memory assemble(byte[] in) {
+        Memory memory = new Memory();
+        if (in.length % 9 != 0) {
+            throw new IllegalArgumentException("Input has wrong length.");
+        }
+        ByteBuffer buffer = ByteBuffer.wrap(in);
+        for (int i = 0; i < buffer.remaining() / 9; i++) {
+            memory.put(buffer.getLong(), buffer.get());
+        }
+        return memory;
+    }
+
+    public void put(long index, byte value) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Index can not be negative");
+        }
+        memory.put(index, value);
+    }
+
+    public byte get(long index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Index can not be negative");
+        }
+        return memory.get(index);
+    }
+
+    public byte[] disassemble() {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES * memory.size() + memory.size());
+
+        memory.forEach((aLong, aByte) -> {
+            buffer.putLong(aLong);
+            buffer.put(aByte);
+        });
+
+        return buffer.array();
+    }
+
+}
---
+++ src/test/java/org/opensecreto/sigmascript/context/item/MemoryTest.java
@@ -0,0 +1,40 @@
+package org.opensecreto.sigmascript.context.item;
+
+import org.assertj.core.api.Assertions;
+import org.opensecreto.sigmascript.Memory;
+import org.testng.annotations.Test;
+
+@Test
+public class MemoryTest {
+
+    public void testPuttingAndGetting() {
+        Memory memory = new Memory();
+        memory.put(21L, (byte) 41);
+        memory.put(48L, (byte) -21);
+
+        Assertions.assertThat(memory.get(0L))
+                .as("Check if non set index returns 0").isEqualTo(0);
+        Assertions.assertThat(memory.get(21L)).isEqualTo((byte) 41);
+        Assertions.assertThat(memory.get(48L))
+                .as("Test getting negative bytes.").isEqualTo((byte) -21);
+    }
+
+    public void testBadArguments() {
+        Memory memory = new Memory();
+        Assertions.assertThatThrownBy(() -> memory.get(-10));
+        Assertions.assertThatThrownBy(() -> memory.put(-10, (byte) 18));
+    }
+
+    public void testAssembling() {
+        Memory memory = new Memory();
+        memory.put(21L, (byte) 41);
+        memory.put(48L, (byte) -21);
+
+        Memory mem = Memory.assemble(memory.disassemble());
+
+        Assertions.assertThat(memory.get(21L)).isEqualTo((byte) 41);
+        Assertions.assertThat(memory.get(48L))
+                .as("Test getting negative bytes.").isEqualTo((byte) -21);
+    }
+
+}
