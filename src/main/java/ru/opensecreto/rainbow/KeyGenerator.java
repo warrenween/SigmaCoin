9745746531ece7ce4ec815f6f59519266f2ee3b8
Mr_Ramych <qwertyppp11@gmail.com>
2017-02-20 10:25:35 +0300

    fixed S generation

--- src/main/java/ru/opensecreto/rainbow/KeyGenerator.java
+++ src/main/java/ru/opensecreto/rainbow/KeyGenerator.java
@@ -4,16 +4,22 @@ package ru.opensecreto.rainbow;
  * Rainbow signature scheme key generator.
  * <p>
  * See: https://pdfs.semanticscholar.org/7977/afcdb8ec9c420935f7a1f8212c303f0ca7fb.pdf
+ * <p>
+ * https://eprint.iacr.org/2010/437.pdf
  */
 public class KeyGenerator {
+
+    private final int[][] K;
 
     private final int[] vi;
     private final int[] oi;
     private final int u;
     private final int n;
-    private final int[] S;
+    private final int[][] S;
 
-    public KeyGenerator(int[] vi) {
+
+    public KeyGenerator(int Kdim, int[] vi) {
+        K = new int[Kdim][Kdim];
 
         if (vi[0] <= 0) {
             throw new IllegalArgumentException("v1 must be greater than 0");
@@ -30,9 +36,11 @@ public class KeyGenerator {
         u = vi.length;
         n = vi[vi.length - 1];
 
-        S = new int[n];
-        for (int i = 0; i < S.length; i++) {
-            S[i] = i + 1;
+        S = new int[u][n];
+        for (int l = 0; l < S.length; l++) {
+            for (int i = 0; i < vi[l]; i++) {
+                S[l][i] = i + 1;
+            }
         }
 
         oi = new int[vi.length - 1];
@@ -57,7 +65,7 @@ public class KeyGenerator {
         return n;
     }
 
-    public int[] getS() {
+    public int[][] getS() {
         return S;
     }
 }
--- src/test/java/ru/opensecreto/rainbowtest/KeyGeneratorTest.java
+++ src/test/java/ru/opensecreto/rainbowtest/KeyGeneratorTest.java
@@ -10,22 +10,24 @@ public class KeyGeneratorTest {
 
     @Test
     public void testIllegalArguments() {
-        assertThatThrownBy(() -> new KeyGenerator(new int[]{-1, 1, 10}));
-        assertThatThrownBy(() -> new KeyGenerator(new int[]{0, 1, 10}));
-        assertThatThrownBy(() -> new KeyGenerator(new int[]{1, 1, 10}));
-        new KeyGenerator(new int[]{1, 2, 3});
+        assertThatThrownBy(() -> new KeyGenerator(256, new int[]{-1, 1, 10}));
+        assertThatThrownBy(() -> new KeyGenerator(256, new int[]{0, 1, 10}));
+        assertThatThrownBy(() -> new KeyGenerator(256, new int[]{1, 1, 10}));
+        new KeyGenerator(256, new int[]{1, 2, 3});
     }
 
     @Test
     public void testParams() {
-        KeyGenerator gen = new KeyGenerator(new int[]{6, 12, 17, 22, 33});
+        KeyGenerator gen = new KeyGenerator(256, new int[]{6, 12, 17, 22, 33});
 
         assertThat(gen.getN()).isEqualTo(33);
-        assertThat(gen.getS()).containsExactly(
-                1, 2, 3, 4, 5, 6, 7, 8, 9,
-                10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                30, 31, 32, 33);
+        assertThat(gen.getS()).containsExactly(new int[][]{
+                {1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33}
+        });
         assertThat(gen.getVi()).containsExactly(6, 12, 17, 22, 33);
         assertThat(gen.getOi()).containsExactly(6, 5, 5, 11);
         assertThat(gen.getU()).isEqualTo(5);
