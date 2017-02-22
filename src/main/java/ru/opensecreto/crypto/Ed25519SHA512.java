cd240d6f12e8886d74172a6fb7b6abf50c9346bc
Mr_Ramych <qwertyppp11@gmail.com>
2017-02-22 20:28:51 +0300

    more ported code

--- src/main/java/ru/opensecreto/crypto/Ed25519SHA512.java
+++ src/main/java/ru/opensecreto/crypto/Ed25519SHA512.java
@@ -21,6 +21,44 @@ public class Ed25519SHA512 implements BaseSigner {
 
     private static BigInteger modp_sqrt_m1 = new BigInteger("19681161376707505956807079304988542015446066515923890162744021073123829784752");
 
+    private static BigInteger g_y = new BigInteger("46316835694926478169428394003475163141307993866256225615783033603165251855960");
+
+    private static BigInteger g_x = new BigInteger("15112221349535400772501151409588531511454012693041857206046113283949847762202");
+
+    private static Point G = new Point(
+            new BigInteger("15112221349535400772501151409588531511454012693041857206046113283949847762202"),
+            new BigInteger("46316835694926478169428394003475163141307993866256225615783033603165251855960"),
+            BigInteger.ONE,
+            new BigInteger("46827403850823179245072216630277197565144205554125654976674165829533817101731")
+    );
+
+    private static BigInteger modp_inv(BigInteger x) {
+        return x.modPow(p.subtract(new BigInteger("2")), p);
+    }
+
+    private static byte[] pointCompress(Point P) {
+        BigInteger zinv = modp_inv(P.z);
+        BigInteger x = P.x.mod(zinv).mod(p);
+        BigInteger y = P.y.mod(zinv).mod(p);
+        return Util.bigToLittleEndian(y.or(x.and(BigInteger.ONE).shiftLeft(255)).toByteArray());
+    }
+
+    private static Point pointDecompress(byte[] s) {
+        if (s.length != 32) {
+            throw new IllegalArgumentException("s length must be 32");
+        }
+        BigInteger y = new BigInteger(Util.bigToLittleEndian(s));
+        BigInteger sign = y.shiftRight(255);
+        y = y.and(new BigInteger("57896044618658097711785492504343953926634992332820282019728792003956564819967"));
+
+        BigInteger x = recoverX(y, sign);
+        if (x == null) {
+            return null;
+        } else {
+            return new Point(x, y, BigInteger.ONE, x.multiply(y).mod(p));
+        }
+    }
+
     private static byte[] sha512(byte[] s) {
         SHA512Digest digest = new SHA512Digest();
         digest.update(s, 0, 32);
@@ -29,16 +67,15 @@ public class Ed25519SHA512 implements BaseSigner {
         return out;
     }
 
-    private static BigInteger secretExpand(byte[] secret) {
+    private static Secret secretExpand(byte[] secret) {
         if (secret.length != 32) {
             throw new IllegalArgumentException("secret must have length of 32");
         }
-        byte[] h = sha512(secret);
-        h = Util.bigToLittleEndian(h);
+        byte[] h = Util.bigToLittleEndian(sha512(secret));
         BigInteger a = new BigInteger(h);
         a = a.and(new BigInteger("28948022309329048855892746252171976963317496166410141009864396001978282409976"));
         a = a.or(new BigInteger("28948022309329048855892746252171976963317496166410141009864396001978282409984"));
-        return a;
+        return new Secret(a, h);
     }
 
     /**
@@ -89,6 +126,31 @@ public class Ed25519SHA512 implements BaseSigner {
         return new Point(E.multiply(F), G.multiply(H), F.multiply(G), E.multiply(H));
     }
 
+    private static BigInteger recoverX(BigInteger y, BigInteger sign) {
+        //x2 = (y*y-1) * modp_inv(d*y*y+1)
+        BigInteger x2 = y.multiply(y).subtract(BigInteger.ONE).multiply(modp_inv(d.multiply(y).multiply(y).add(BigInteger.ONE)));
+        if (x2.compareTo(BigInteger.ZERO) == 0) {
+            if (sign.compareTo(BigInteger.ZERO) == 0) {
+                return null;
+            } else {
+                return BigInteger.ZERO;
+            }
+        }
+
+        BigInteger x = x2.modPow(new BigInteger("57896044618658097711785492504343953926634992332820282019728792003956564819952"), p);
+        if (x.multiply(x).subtract(x2).mod(p).compareTo(BigInteger.ZERO) != 0) {
+            x = x.multiply(modp_sqrt_m1).mod(p);
+        }
+        if (x.multiply(x).subtract(x2).mod(p).compareTo(BigInteger.ZERO) != 0) {
+            return null;
+        }
+
+        if (x.and(BigInteger.ONE).compareTo(sign) != 0) {
+            x = x.subtract(p);
+        }
+        return x;
+    }
+
     /**
      * Generate public key from 32 bytes of private key.
      * <p>
@@ -100,18 +162,8 @@ public class Ed25519SHA512 implements BaseSigner {
      * @return public key for this key
      */
     public static byte[] getPublicKey(byte[] secret) {
-
-        SHA512Digest digest = new SHA512Digest();
-        digest.update(secret, 0, 32);
-        byte[] h = new byte[64];
-        digest.doFinal(h, 0);
-
-        h[0] &= 0x07;
-        h[31] &= 0x7F;
-        h[31] |= 0x40;
-
-
-        return new byte[0];
+        BigInteger a = secretExpand(secret).v;
+        return pointCompress(pointMultiply(a, G));
     }
 
     @Override
@@ -135,6 +187,15 @@ public class Ed25519SHA512 implements BaseSigner {
             this.y = y;
             this.z = z;
             this.t = t;
+        }
+    }
+
+    private static class Secret {
+        public BigInteger v;
+        public byte[] arr;
+
+        public Secret(BigInteger a, byte[] h) {
+
         }
     }
 }
