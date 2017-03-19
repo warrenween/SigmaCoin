8fb6e5b370ee47ec314a3a384c65f6944ba8329d
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-19 19:27:08 +0300

    removed gui subproject (this will be in separate repo)

--- desktopgui/src/main/java/ru/opensecreto/sigmacoin/desktopgui/DesktopGuiConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.opensecreto.sigmacoin.desktopgui;
-
-import ru.opensecreto.sigmacoin.core.config.CoinConfig;
-
-public class DesktopGuiConfig {
-
-    public static final String TITLE = CoinConfig.COIN_NAME + " wallet";
-}
