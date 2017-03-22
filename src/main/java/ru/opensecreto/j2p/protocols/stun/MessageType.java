6b576e8e9b8f3a154b0cf8a7d40d7cffba10a71e
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-22 20:32:56 +0300

    message type

---
+++ src/main/java/ru/opensecreto/j2p/protocols/stun/MessageType.java
@@ -0,0 +1,21 @@
+package ru.opensecreto.j2p.protocols.stun;
+
+/**
+ * Defined by RFC5389, p 10-11
+ * <p>
+ * Values are already encoded to structure of header.
+ */
+public class MessageType {
+
+    //type bits
+    //                 *       *
+    public static final byte[] REQUEST =
+            {0b0000_0000, 0b0000_0000};
+    public static final byte[] SUCCESS =
+            {0b0000_0001, 0b0000_0000};
+    public static final byte[] FAILURE =
+            {0b0000_0001, 0b0001_0000};
+    public static final byte[] INDICATION =
+            {0b0000_0000, 0b0001_0000};
+
+}
