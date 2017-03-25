4dff669bf5f02ad5e76c7f32615e2452b49c1b55
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-25 17:58:34 +0300

    improved code

--- src/test/java/ru/opensecreto/j2p/protocol/stun/StunMessageTest.java
+++ src/test/java/ru/opensecreto/j2p/protocol/stun/StunMessageTest.java
@@ -8,52 +8,55 @@ import ru.opensecreto.j2p.protocols.stun.StunMessage;
 
 import javax.xml.bind.DatatypeConverter;
 
+import static javax.xml.bind.DatatypeConverter.parseHexBinary;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StunMessageTest {
 
     @Test
     public void testCreatingMessage() {
-        Assertions.assertThat(new StunMessage(
+        assertThat(new StunMessage(
                 MessageClass.REQUEST_BYTES,
                 MessageMethod.BINDING_BYTES,
-                DatatypeConverter.parseHexBinary("64744968693676426f393366"),
-                DatatypeConverter.parseHexBinary("802f0016687474703a2f2f6c" +
+                parseHexBinary("64744968693676426f393366"),
+                parseHexBinary("802f0016687474703a2f2f6c" +
                         "6f63616c686f73743a333030302f0000")
         ).getStunMessage()).inHexadecimal().containsExactly(
-                DatatypeConverter.parseHexBinary("0001001c2112a4426474496869367642" +
+                parseHexBinary("0001001c2112a4426474496869367642" +
                         "6f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000")
         );
-        Assertions.assertThat(new StunMessage(
+        assertThat(new StunMessage(
                 MessageClass.REQUEST,
                 MessageMethod.BINDING,
-                DatatypeConverter.parseHexBinary("64744968693676426f393366"),
-                DatatypeConverter.parseHexBinary("802f0016687474703a2f2f6c" +
+                parseHexBinary("64744968693676426f393366"),
+                parseHexBinary("802f0016687474703a2f2f6c" +
                         "6f63616c686f73743a333030302f0000")
         ).getStunMessage()).inHexadecimal().containsExactly(
-                DatatypeConverter.parseHexBinary("0001001c2112a4426474496869367642" +
+                parseHexBinary("0001001c2112a4426474496869367642" +
                         "6f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000")
         );
     }
 
     @Test
     public void testCreatingFromBytes() {
-        Assertions.assertThat(new StunMessage(DatatypeConverter.parseHexBinary(
+        assertThat(new StunMessage(parseHexBinary(
                 "0001001c2112a44264744968693676426f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
-        )).getStunMessage()).inHexadecimal().containsExactly(DatatypeConverter.parseHexBinary(
+        )).getStunMessage()).inHexadecimal().containsExactly(parseHexBinary(
                 "0001001c2112a44264744968693676426f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
         ));
     }
 
     @Test
     public void testGettingData() {
-        Assertions.assertThat(new StunMessage(DatatypeConverter.parseHexBinary(
+        assertThat(new StunMessage(parseHexBinary(
                 "0001001c2112a44264744968693676426f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
         )).getMessageClass()).isEqualTo(MessageClass.REQUEST);
-        Assertions.assertThat(new StunMessage(DatatypeConverter.parseHexBinary(
+        assertThat(new StunMessage(parseHexBinary(
                 "0001001c2112a44264744968693676426f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
         )).getMessageMethod()).isEqualTo(MessageMethod.BINDING);
-        Assertions.assertThat(new StunMessage(DatatypeConverter.parseHexBinary(
+        assertThat(new StunMessage(parseHexBinary(
                 "0001001c2112a44264744968693676426f393366802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
-        )).getData()).isEqualTo(DatatypeConverter.parseHexBinary(
+        )).getData()).isEqualTo(parseHexBinary(
                 "802f0016687474703a2f2f6c6f63616c686f73743a333030302f0000"
         ));
     }
