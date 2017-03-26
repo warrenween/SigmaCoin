08cf6b66fd53be1e383970fa5c046f8cfbc691a6
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-26 14:37:39 +0300

    fixed null pointer exception if agent execution failed

--- sandbox/src/main/java/TestGettingIpFromStun.java
+++ sandbox/src/main/java/TestGettingIpFromStun.java
@@ -2,6 +2,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.opensecreto.j2p.protocols.stun.*;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -18,16 +19,16 @@ public class TestGettingIpFromStun {
         return Servers.servers.get(new Random().nextInt(Servers.servers.size()));
     }
 
-    public void run() throws InterruptedException {
+    public void run() throws InterruptedException, UnknownHostException {
         List<Future<StunMessage>> messages = new ArrayList<>(5);
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             StunAgent agent = new StunAgent(getRandomServer(), new StunMessage(
                     MessageClass.REQUEST,
                     MessageMethod.BINDING,
                     new byte[12],
                     Collections.EMPTY_LIST
-            ));
+            ), 1);
             messages.add(executorService.submit(agent));
         }
 
@@ -39,8 +40,8 @@ public class TestGettingIpFromStun {
                 return;
             } else {
                 messages.forEach(stunMessageFuture -> {
-                    if (stunMessageFuture.isDone()) {
-                        try {
+                    try {
+                        if (stunMessageFuture.isDone() && stunMessageFuture.get() != null) {
                             StunMessage message = stunMessageFuture.get();
                             List<Attribute> attributes = Attribute.decode(message.getData());
                             attributes.forEach(attribute -> {
@@ -54,10 +55,10 @@ public class TestGettingIpFromStun {
                                     LOGGER.error("Could not parse ip", e);
                                 }
                             });
-                        } catch (Throwable e) {
-                            LOGGER.error("Could not finish task", e);
                             messages.remove(stunMessageFuture);
                         }
+                    } catch (Throwable e) {
+                        LOGGER.error("Could not finish task", e);
                     }
                 });
             }
