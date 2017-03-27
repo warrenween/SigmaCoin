ce75feb318e9fd1b5a4632ab064026b1ebff38b8
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-27 19:19:12 +0300

    some code for handling connections

--- src/main/java/ru/opensecreto/j2p/ConnectionHandler.java
+++ src/main/java/ru/opensecreto/j2p/ConnectionHandler.java
@@ -1,8 +1,16 @@
 package ru.opensecreto.j2p;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ConnectionHandler implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionHandler.class);
 
     private final Socket socket;
     private final Controller controller;
@@ -14,6 +22,22 @@ public class ConnectionHandler implements Runnable {
 
     @Override
     public void run() {
-
+        InputStream in;
+        OutputStream out;
+        try {
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+        } catch (IOException e) {
+            LOGGER.error("Could not create streams", e);
+            try {
+                socket.close();
+            } catch (IOException e1) {
+                LOGGER.error("Could not close socket.", e);
+            }
+            Thread.currentThread().interrupt();
+        }
+        while (!Thread.currentThread().isInterrupted() && socket.isConnected() && !socket.isClosed()) {
+            
+        }
     }
 }
