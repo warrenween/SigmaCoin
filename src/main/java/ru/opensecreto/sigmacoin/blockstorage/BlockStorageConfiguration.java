f52c490c2a17c3e0699fe93f7286093439fd222e
Mr_Ramych <qwertyppp11@gmail.com>
2017-03-29 21:23:47 +0300

    fixed typo

--- src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfiguration.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfiguration.java
@@ -4,13 +4,13 @@ import java.io.File;
 
 public class BlockStorageConfiguration {
 
-    public final File blockchainFoler;
+    public final File blockchainFolder;
     public final int hashLength;
     public final int blockMaxSize;
     public final int maxBlocksPerFile;
 
-    public BlockStorageConfiguration(File blockchainFoler, int hashLength, int blockMaxSize, int maxBlocksPerFile) {
-        this.blockchainFoler = blockchainFoler;
+    public BlockStorageConfiguration(File blockchainFolder, int hashLength, int blockMaxSize, int maxBlocksPerFile) {
+        this.blockchainFolder = blockchainFolder;
         this.hashLength = hashLength;
         this.blockMaxSize = blockMaxSize;
         this.maxBlocksPerFile = maxBlocksPerFile;
