3df02beab7adf59b1ab5ae70e371995f8fe02f5b
Mr_Ramych <qwertyppp11@gmail.com>
2017-04-01 14:17:58 +0300

    BlockStorage: checking hash and block sizes

--- src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageController.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageController.java
@@ -14,16 +14,24 @@ public class BlockStorageController {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(BlockStorageController.class);
 
+    private final int hashSize;
+    private final int maxBlockSize;
+
     private DB db;
     private HTreeMap<byte[], byte[]> blocks;
 
     public BlockStorageController(File file, int hashSize, int maxBlockSize) {
+        this.hashSize = hashSize;
+        this.maxBlockSize = maxBlockSize;
+
         db = DBMaker.fileDB(file).transactionEnable().closeOnJvmShutdown().make();
         blocks = db.hashMap("blocks", Serializer.BYTE_ARRAY, Serializer.BYTE_ARRAY)
                 .counterEnable().createOrOpen();
     }
 
     public boolean addBlock(byte[] hash, byte[] data) {
+        checkHash(hash);
+        checkBlock(data);
         if (!blocks.containsKey(hash)) {
             blocks.put(hash, data);
             return true;
@@ -32,11 +40,25 @@ public class BlockStorageController {
     }
 
     public boolean hasBlock(byte[] hash) {
+        checkHash(hash);
         return blocks.containsKey(hash);
     }
 
     public byte[] getBlock(byte[] hash) {
+        checkHash(hash);
         return blocks.get(hash);
+    }
+
+    private void checkHash(byte[] hash) {
+        if (hash.length != hashSize) throw new IllegalArgumentException(
+                "Incorrect hash length. Required " + hashSize + " bytes. Got " + hash.length + " bytes."
+        );
+    }
+
+    private void checkBlock(byte[] data) {
+        if (data.length > maxBlockSize) throw new IllegalArgumentException(
+                "Block is too big. Max " + maxBlockSize + " bytes. Given " + data.length + " bytes."
+        );
     }
 
 }
