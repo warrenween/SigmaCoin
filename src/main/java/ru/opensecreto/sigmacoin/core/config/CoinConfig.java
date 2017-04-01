6c8bd53a033fab3696bc1a3b9532a3c90d46a68c
Mr_Ramych <qwertyppp11@gmail.com>
2017-04-01 13:30:23 +0300

    config is final

--- src/main/java/ru/opensecreto/sigmacoin/core/config/CoinConfig.java
+++ src/main/java/ru/opensecreto/sigmacoin/core/config/CoinConfig.java
@@ -2,7 +2,7 @@ package ru.opensecreto.sigmacoin.core.config;
 
 public class CoinConfig {
 
-    public static String COIN_NAME = "Sigmacoin";
-    public static String COIN_SHORT_NAME = "SGC";
+    public static final String COIN_NAME = "Sigmacoin";
+    public static final String COIN_SHORT_NAME = "SGC";
 
 }
