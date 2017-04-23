5c0dbbda9fd310491c208b5ebf159bfe7c5c13e2
Mr_Ramych <qwertyppp11@gmail.com>
2017-04-23 21:42:56 +0300

    removed port configuration in time synchronizer
    * commons-net do not allow to change ntp port

--- src/main/java/ru/opensecreto/sigmacoin/time/TimeSyncronizer.java
+++ src/main/java/ru/opensecreto/sigmacoin/time/TimeSyncronizer.java
@@ -7,30 +7,10 @@ import org.slf4j.LoggerFactory;
 public class TimeSyncronizer {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(TimeSyncronizer.class);
-    private final static int defaultNTPport = 123;
-    private final static int NTPport;
 
-    private final int port;
     private final Time time;
 
-    static {
-        int tempPort;
-        if (System.getProperty("time.ntpport") == null) {
-            LOGGER.info("System property 'time.ntpport' is not defined. Using default {}.", defaultNTPport);
-            tempPort = defaultNTPport;
-        } else {
-            try {
-                tempPort = Integer.parseInt(System.getProperty("time.ntpport"));
-            } catch (NumberFormatException e) {
-                LOGGER.warn("Could not parse property 'time.ntpport'", e);
-                tempPort = defaultNTPport;
-            }
-        }
-        NTPport = tempPort;
-    }
-
-    public TimeSyncronizer(int port, Time time) {
-        this.port = port;
+    public TimeSyncronizer(Time time) {
         this.time = time;
     }
 }
