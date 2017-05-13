1bfb8a5455ab4cabd92c1cf39299b4ca0fdf9a0d
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-13 21:33:42 +0300

    fixed modifiers of digestprovider in config

--- src/main/java/ru/opensecreto/sigmacoin/core/config/CoinConfig.java
+++ src/main/java/ru/opensecreto/sigmacoin/core/config/CoinConfig.java
@@ -23,7 +23,7 @@ public class CoinConfig {
      */
     public static final int HASH_LENGTH = 64;
 
-    public DigestProvider POW_DIGEST = () -> new SHA3Digest(512);
+    public final static DigestProvider POW_DIGEST = () -> new SHA3Digest(512);
 
     public final static int POW_N = 3;
 
