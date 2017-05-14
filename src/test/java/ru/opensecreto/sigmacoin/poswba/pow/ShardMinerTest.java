a20b005f35cd4fb73c87ac4426df66e2ee876579
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-14 14:37:16 +0300

    adjusted test params

--- src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
@@ -19,7 +19,7 @@ public class ShardMinerTest {
         DigestProvider provider = () -> new ShortenedDigest(new MD2Digest(), 2);
         Ed25519PublicKey publicKey = (Ed25519PublicKey) new Ed25519KeyGenerator().generateKeypair().getPublicKey();
 
-        ShardMiner miner = new ShardMiner(publicKey, 134217728, provider, 3);
+        ShardMiner miner = new ShardMiner(publicKey, 1000000, provider, 3);
 
         Shard shard = miner.call();
 
