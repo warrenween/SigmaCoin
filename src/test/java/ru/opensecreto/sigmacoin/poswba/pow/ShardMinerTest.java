15df838b4dcabb886e9b7e1240949a594244bb80
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-14 14:22:22 +0300

    shortening hash legally

--- src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
@@ -2,8 +2,11 @@ package ru.opensecreto.sigmacoin.poswba.pow;
 
 import com.google.common.primitives.Longs;
 import org.assertj.core.api.Assertions;
-import org.bouncycastle.crypto.digests.KeccakDigest;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.digests.MD2Digest;
+import org.bouncycastle.crypto.digests.ShortenedDigest;
 import org.testng.annotations.Test;
+import ru.opensecreto.sigmacoin.core.DigestProvider;
 import ru.opensecreto.sigmacoin.crypto.Ed25519.Ed25519KeyGenerator;
 import ru.opensecreto.sigmacoin.crypto.Ed25519.Ed25519PublicKey;
 import ru.opensecreto.sigmacoin.poswba.storage.Shard;
@@ -13,18 +16,19 @@ public class ShardMinerTest {
 
     @Test
     public void testShardMining() throws Exception {
+        DigestProvider provider = () -> new ShortenedDigest(new MD2Digest(), 2);
         Ed25519PublicKey publicKey = (Ed25519PublicKey) new Ed25519KeyGenerator().generateKeypair().getPublicKey();
 
-        ShardMiner miner = new ShardMiner(publicKey, 134217728, () -> new KeccakDigest(16), 3);
+        ShardMiner miner = new ShardMiner(publicKey, 134217728, provider, 3);
 
         Shard shard = miner.call();
 
         Assertions.assertThat(shard.publicKey).isEqualTo(publicKey);
 
         //checking if solution valid
-        byte[] result = new byte[new KeccakDigest(16).getDigestSize()];
+        byte[] result = new byte[provider.getDigest().getDigestSize()];
 
-        KeccakDigest digest = new KeccakDigest(16);
+        Digest digest = provider.getDigest();
         digest.update(Longs.toByteArray(shard.getIds()[0]), 0, 8);
         digest.update(shard.getSeed(), 0, 64);
         digest.update(publicKey.getPublicKey(), 0, publicKey.getPublicKey().length);
@@ -34,7 +38,7 @@ public class ShardMinerTest {
         digest.update(Longs.toByteArray(shard.getIds()[1]), 0, 8);
         digest.update(shard.getSeed(), 0, 64);
         digest.update(publicKey.getPublicKey(), 0, publicKey.getPublicKey().length);
-        byte[] tmp = new byte[new KeccakDigest(16).getDigestSize()];
+        byte[] tmp = new byte[provider.getDigest().getDigestSize()];
         digest.doFinal(tmp, 0);
 
         //xor
