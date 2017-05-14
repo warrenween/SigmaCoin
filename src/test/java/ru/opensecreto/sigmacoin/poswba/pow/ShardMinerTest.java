7b863edfc890bfc3449a38d605964fd6c9c7becf
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-14 14:14:43 +0300

    modified test params

--- src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/poswba/pow/ShardMinerTest.java
@@ -2,7 +2,7 @@ package ru.opensecreto.sigmacoin.poswba.pow;
 
 import com.google.common.primitives.Longs;
 import org.assertj.core.api.Assertions;
-import org.bouncycastle.crypto.digests.MD5Digest;
+import org.bouncycastle.crypto.digests.KeccakDigest;
 import org.testng.annotations.Test;
 import ru.opensecreto.sigmacoin.crypto.Ed25519.Ed25519KeyGenerator;
 import ru.opensecreto.sigmacoin.crypto.Ed25519.Ed25519PublicKey;
@@ -15,16 +15,16 @@ public class ShardMinerTest {
     public void testShardMining() throws Exception {
         Ed25519PublicKey publicKey = (Ed25519PublicKey) new Ed25519KeyGenerator().generateKeypair().getPublicKey();
 
-        ShardMiner miner = new ShardMiner(publicKey, 16777216, MD5Digest::new, 3);
+        ShardMiner miner = new ShardMiner(publicKey, 134217728, () -> new KeccakDigest(16), 3);
 
         Shard shard = miner.call();
 
         Assertions.assertThat(shard.publicKey).isEqualTo(publicKey);
 
         //checking if solution valid
-        byte[] result = new byte[new MD5Digest().getDigestSize()];
+        byte[] result = new byte[new KeccakDigest(16).getDigestSize()];
 
-        MD5Digest digest = new MD5Digest();
+        KeccakDigest digest = new KeccakDigest(16);
         digest.update(Longs.toByteArray(shard.getIds()[0]), 0, 8);
         digest.update(shard.getSeed(), 0, 64);
         digest.update(publicKey.getPublicKey(), 0, publicKey.getPublicKey().length);
@@ -34,7 +34,7 @@ public class ShardMinerTest {
         digest.update(Longs.toByteArray(shard.getIds()[1]), 0, 8);
         digest.update(shard.getSeed(), 0, 64);
         digest.update(publicKey.getPublicKey(), 0, publicKey.getPublicKey().length);
-        byte[] tmp = new byte[new MD5Digest().getDigestSize()];
+        byte[] tmp = new byte[new KeccakDigest(16).getDigestSize()];
         digest.doFinal(tmp, 0);
 
         //xor
