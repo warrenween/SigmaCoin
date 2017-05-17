9322d61d85c3d01ab1c885d8bcb358547134d593
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-17 18:36:20 +0300

    simple opcode

---
+++ src/main/java/ru/opensecreto/sigmacoin/vm/Opcodes.java
@@ -0,0 +1,12 @@
+package ru.opensecreto.sigmacoin.vm;
+
+import java.util.List;
+
+public class Opcodes {
+
+    /**
+     * Stop execution. Nothing is reverted.
+     */
+    public static final byte STOP = 0x00;
+    
+}
