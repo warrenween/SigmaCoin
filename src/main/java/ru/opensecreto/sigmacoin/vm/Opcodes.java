6480939c532ce3310bc799e691c2a48fe0657fc5
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-17 19:35:51 +0300

    bad STOP + good STOP

--- src/main/java/ru/opensecreto/sigmacoin/vm/Opcodes.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/Opcodes.java
@@ -1,12 +1,12 @@
 package ru.opensecreto.sigmacoin.vm;
 
-import java.util.List;
-
 public class Opcodes {
 
     /**
      * Stop execution. Nothing is reverted.
      */
-    public static final byte STOP = 0x00;
-    
+    public static final byte STOP_BAD = 0x00;
+
+    public static final byte STOP_GOOD = 0x01;
+
 }
