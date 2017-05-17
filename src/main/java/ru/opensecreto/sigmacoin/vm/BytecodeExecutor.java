980dd0258fc81e592d763902caa85f470e08f84b
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-17 19:36:07 +0300

    basic bytecode executor

--- src/main/java/ru/opensecreto/sigmacoin/vm/BytecodeExecutor.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/BytecodeExecutor.java
@@ -9,7 +9,34 @@ public class BytecodeExecutor {
         this.frame = frame;
     }
 
-    public void run() {
+    public Stack run() {
+        boolean run = true;
+        boolean success = true;
+        while (run) {
+            switch (frame.memory.get(pointer)) {
+                case Opcodes.STOP_BAD:
+                    run = false;
+                    success = false;
+                    break;
 
+
+                case Opcodes.STOP_GOOD:
+                    run = false;
+                    break;
+
+
+                default:
+                    run = false;
+                    success = false;
+            }
+        }
+
+        if (success) {
+            frame.stack.push((byte) 0x00);
+        } else {
+            frame.stack.push((byte) 0x01);
+        }
+
+        return frame.stack;
     }
 }
