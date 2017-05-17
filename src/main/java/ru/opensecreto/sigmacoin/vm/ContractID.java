c11d32cf80ab0d4ddcba752117fa94579092f918
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-17 19:46:32 +0300

    ContactId.toString();

--- src/main/java/ru/opensecreto/sigmacoin/vm/ContractID.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/ContractID.java
@@ -1,5 +1,6 @@
 package ru.opensecreto.sigmacoin.vm;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.Arrays;
 
 public class ContractID {
@@ -12,5 +13,10 @@ public class ContractID {
 
     public byte[] getId() {
         return Arrays.copyOf(id, id.length);
+    }
+
+    @Override
+    public String toString() {
+        return "0x" + DatatypeConverter.printHexBinary(id);
     }
 }
