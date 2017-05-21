78c40bcd9c0da96dba79d5b41356c9c9a8d4769f
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-21 19:16:13 +0300

    maxMemorySize is specified in VMConfiguration

--- src/main/java/ru/opensecreto/sigmacoin/vm/VMConfiguration.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/VMConfiguration.java
@@ -5,14 +5,17 @@ public class VMConfiguration {
     public final int frameMaxStackSize;
     public final int contractIdLength;
     public final int maxCallDepth;
+    public final int memoryMaxSize;
 
-    public VMConfiguration(int frameMaxStackSize, int contractIdLength, int maxCallDepth) {
+    public VMConfiguration(int frameMaxStackSize, int contractIdLength, int maxCallDepth, int memoryMaxSize) {
         if (maxCallDepth <= 0) throw new IllegalArgumentException("maxCallDepth must be >= 1");
         if (contractIdLength <= 0) throw new IllegalArgumentException("contractIdLength must be >= 1");
-        if (frameMaxStackSize <= 0) throw new IllegalArgumentException("frameMaxStack size must be >= 1");
+        if (frameMaxStackSize <= 0) throw new IllegalArgumentException("frameMaxStack must be >= 1");
+        if (memoryMaxSize < 1) throw new IllegalArgumentException("memoryMaxSize must be >= 1")
 
         this.frameMaxStackSize = frameMaxStackSize;
         this.contractIdLength = contractIdLength;
         this.maxCallDepth = maxCallDepth;
+        this.memoryMaxSize = memoryMaxSize;
     }
 }
--- src/test/java/ru/opensecreto/sigmacoin/vm/VMConfigurationTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/vm/VMConfigurationTest.java
@@ -7,36 +7,42 @@ public class VMConfigurationTest {
 
     @Test
     public void test() {
-        VMConfiguration configuration = new VMConfiguration(28, 33, 54);
+        VMConfiguration configuration = new VMConfiguration(28, 33, 54, 94);
         Assertions.assertThat(configuration.frameMaxStackSize).isEqualTo(28);
         Assertions.assertThat(configuration.contractIdLength).isEqualTo(33);
         Assertions.assertThat(configuration.maxCallDepth).isEqualTo(54);
+        Assertions.assertThat(configuration.memoryMaxSize).isEqualTo(94);
     }
 
     @Test
     public void testNearlyBadValues() {
-        VMConfiguration configuration = new VMConfiguration(1, 1, 1);
+        VMConfiguration configuration = new VMConfiguration(1, 1, 1, 1);
         Assertions.assertThat(configuration.frameMaxStackSize).isEqualTo(1);
         Assertions.assertThat(configuration.contractIdLength).isEqualTo(1);
         Assertions.assertThat(configuration.maxCallDepth).isEqualTo(1);
+        Assertions.assertThat(configuration.memoryMaxSize).isEqualTo(1);
     }
 
     @Test
     public void testBadValues() {
         //test zeroes
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(0, 1, 1))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(0, 1, 1, 1))
                 .isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(1, 0, 1))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(1, 0, 1, 1))
                 .isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(1, 1, 0))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(1, 1, 0, 1))
+                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(1, 1, 1, 0))
                 .isInstanceOf(IllegalArgumentException.class);
 
         //test negatives
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(-10, 10, 10))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(-10, 10, 10, 10))
                 .isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(10, -10, 10))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(10, -10, 10, 10))
                 .isInstanceOf(IllegalArgumentException.class);
-        Assertions.assertThatThrownBy(() -> new VMConfiguration(10, 10, -10))
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(10, 10, -10, 10))
+                .isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new VMConfiguration(10, 10, 10, -10))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
