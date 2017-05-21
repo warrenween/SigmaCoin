a64d05166d2b9f87aa5dc23b495df8bc5feae1bd
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-21 22:04:02 +0300

    testing stack opcodes

--- src/test/java/ru/opensecreto/sigmacoin/vm/BytecodeExecutorTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/vm/BytecodeExecutorTest.java
@@ -85,4 +85,30 @@ public class BytecodeExecutorTest {
         assertThat(result.popShort()).isEqualTo((short) 4);
         assertThat(result.popInt()).isEqualTo(0x1f1f1f1f);
     }
+
+    @Test
+    public void testPUSH_POP_DUP() {
+        ContractID idA = new ContractID(new byte[]{0});
+        Memory contractA = mock(Memory.class);
+        when(contractA.get(0)).thenReturn(Opcodes.PUSH);
+        when(contractA.get(1)).thenReturn((byte) 0x1f);
+        when(contractA.get(2)).thenReturn(Opcodes.PUSH);
+        when(contractA.get(3)).thenReturn((byte) 0x56);
+        when(contractA.get(4)).thenReturn(Opcodes.DUP);
+        when(contractA.get(5)).thenReturn(Opcodes.POP);
+        when(contractA.get(6)).thenReturn(Opcodes.STOP_GOOD);
+
+        ContractManager manager = mock(ContractManager.class);
+        when(manager.contractExists(idA)).thenReturn(true);
+        when(manager.getContract(idA)).thenReturn(contractA);
+
+        VirtualMachineController controller = new VirtualMachineController(manager,
+                new VMConfiguration(10, 1, 10, 10));
+
+        Stack result = controller.invoke(new Stack(0), idA);
+
+        assertThat(result.pop()).isEqualTo((byte) 0x00);
+        assertThat(result.popShort()).isEqualTo((short) 2);
+        assertThat(result.popShort()).isEqualTo(0x561f);
+    }
 }
