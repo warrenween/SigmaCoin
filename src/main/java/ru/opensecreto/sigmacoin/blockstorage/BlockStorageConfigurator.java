81614b2af745e22b69984493497513d1b47c1a30
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-22 12:19:20 +0300

    moved blockstorage constants to separate class

--- src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfigurator.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfigurator.java
@@ -2,19 +2,15 @@ package ru.opensecreto.sigmacoin.blockstorage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.opensecreto.sigmacoin.config.EnvironmentConfig;
 
 public class BlockStorageConfigurator {
 
-    /**
-     * 128 mib
-     */
-    public static final int DEFAULT_START_SIZE = 134217728;
-    public static final int DEFAULT_ALLOCATE_SIZE = 67108864;
     private static final Logger LOGGER = LoggerFactory.getLogger(BlockStorageConfigurator.class);
 
     public static BlockStorageConfiguration create() {
-        LOGGER.info("Setting default START_SIZE {}.", DEFAULT_START_SIZE);
-        int startSize = DEFAULT_START_SIZE;
+        LOGGER.info("Setting default START_SIZE {}.", EnvironmentConfig.BLOCK_STORAGE_DEFAULT_START_SIZE);
+        int startSize = EnvironmentConfig.BLOCK_STORAGE_DEFAULT_START_SIZE;
         if (System.getProperty("blockstorage.startsize") != null) {
             try {
                 startSize = Integer.parseUnsignedInt(System.getProperty("blockstorage.startsize"));
@@ -23,8 +19,8 @@ public class BlockStorageConfigurator {
             }
         }
 
-        LOGGER.info("Setting default ALLOCATE_START_SIZE {}.", DEFAULT_ALLOCATE_SIZE);
-        int allocateSize = DEFAULT_ALLOCATE_SIZE;
+        LOGGER.info("Setting default ALLOCATE_START_SIZE {}.", EnvironmentConfig.BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE);
+        int allocateSize = EnvironmentConfig.BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE;
         if (System.getProperty("blockstorage.allocatesize") != null) {
             try {
                 allocateSize = Integer.parseUnsignedInt(System.getProperty("blockstorage.allocatesize"));
---
+++ src/main/java/ru/opensecreto/sigmacoin/config/EnvironmentConfig.java
@@ -0,0 +1,10 @@
+package ru.opensecreto.sigmacoin.config;
+
+public class EnvironmentConfig {
+
+    /**
+     * 128 mib
+     */
+    public static final int BLOCK_STORAGE_DEFAULT_START_SIZE = 134217728;
+    public static final int BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE = 67108864;
+}
