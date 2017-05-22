b26a978ba1a8c6f92c0e77503060bfbe2047e1ac
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-22 19:38:41 +0300

    moved constants

--- src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfigurator.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockstorage/BlockStorageConfigurator.java
@@ -2,6 +2,7 @@ package ru.opensecreto.sigmacoin.blockstorage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.opensecreto.sigmacoin.config.Constants;
 import ru.opensecreto.sigmacoin.config.EnvironmentConfig;
 
 public class BlockStorageConfigurator {
@@ -11,21 +12,21 @@ public class BlockStorageConfigurator {
     public static BlockStorageConfiguration create() {
         LOGGER.info("Setting default START_SIZE {}.", EnvironmentConfig.BLOCK_STORAGE_DEFAULT_START_SIZE);
         int startSize = EnvironmentConfig.BLOCK_STORAGE_DEFAULT_START_SIZE;
-        if (System.getProperty("blockstorage.startsize") != null) {
+        if (System.getProperty(Constants.BLOCK_STORAGE_START_SIZE_PROPERTY) != null) {
             try {
-                startSize = Integer.parseUnsignedInt(System.getProperty("blockstorage.startsize"));
+                startSize = Integer.parseUnsignedInt(System.getProperty(Constants.BLOCK_STORAGE_START_SIZE_PROPERTY));
             } catch (NumberFormatException e) {
-                LOGGER.warn("Error parsing 'blockstorage.startsize' property.", e);
+                LOGGER.warn("Error parsing '{}' property.", Constants.BLOCK_STORAGE_START_SIZE_PROPERTY, e);
             }
         }
 
         LOGGER.info("Setting default ALLOCATE_START_SIZE {}.", EnvironmentConfig.BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE);
         int allocateSize = EnvironmentConfig.BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE;
-        if (System.getProperty("blockstorage.allocatesize") != null) {
+        if (System.getProperty(Constants.BLOCK_STORAGE_ALLOCATE_SIZE_PROPERTY) != null) {
             try {
-                allocateSize = Integer.parseUnsignedInt(System.getProperty("blockstorage.allocatesize"));
+                allocateSize = Integer.parseUnsignedInt(System.getProperty(Constants.BLOCK_STORAGE_ALLOCATE_SIZE_PROPERTY));
             } catch (NumberFormatException e) {
-                LOGGER.warn("Error parsing 'blockstorage.allocatesize' property.", e);
+                LOGGER.warn("Error parsing '{}' property.", Constants.BLOCK_STORAGE_ALLOCATE_SIZE_PROPERTY, e);
             }
         }
 
---
+++ src/main/java/ru/opensecreto/sigmacoin/config/Constants.java
@@ -0,0 +1,6 @@
+package ru.opensecreto.sigmacoin.config;
+
+public class Constants {
+    public static final String BLOCK_STORAGE_START_SIZE_PROPERTY = "blockstorage.startsize";
+    public static final String BLOCK_STORAGE_ALLOCATE_SIZE_PROPERTY = "blockstorage.allocatesize";
+}
