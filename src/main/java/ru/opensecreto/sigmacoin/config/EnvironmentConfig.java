3cdc6399d6f4582bd303b7cc3a9680743a1afabd
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-22 12:24:21 +0300

    block storage constants docs

--- src/main/java/ru/opensecreto/sigmacoin/config/EnvironmentConfig.java
+++ src/main/java/ru/opensecreto/sigmacoin/config/EnvironmentConfig.java
@@ -3,8 +3,11 @@ package ru.opensecreto.sigmacoin.config;
 public class EnvironmentConfig {
 
     /**
-     * 128 mib
+     * Default size of block storage database upon creation. 128MiB.
      */
     public static final int BLOCK_STORAGE_DEFAULT_START_SIZE = 134217728;
+    /**
+     * How much increase block storage database when it is full. 64 MiB
+     */
     public static final int BLOCK_STORAGE_DEFAULT_ALLOCATE_SIZE = 67108864;
 }
