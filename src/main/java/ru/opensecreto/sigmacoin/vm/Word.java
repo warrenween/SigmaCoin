9ae8c7ad3e751830427fa771ec83e4ecf5be1e6b
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-26 11:42:18 +0300

    some constructors

--- src/main/java/ru/opensecreto/sigmacoin/vm/Word.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/Word.java
@@ -1,10 +1,12 @@
 package ru.opensecreto.sigmacoin.vm;
 
+import com.google.common.primitives.Shorts;
+
 import javax.xml.bind.DatatypeConverter;
 import java.util.Arrays;
 
 /**
- * Word is 32 byte (256 bits) value.
+ * Word is 32 byte (256 bits) signed value. Most significant
  */
 public final class Word {
 
@@ -18,6 +20,14 @@ public final class Word {
     public Word(byte[] data) {
         if (data.length != 32) throw new IllegalArgumentException("data length must be 32");
         System.arraycopy(data, 0, this.data, 0, WORD_SIZE);
+    }
+
+    public Word(byte value) {
+        data[31] = value;
+    }
+
+    public Word(short value) {
+        System.arraycopy(Shorts.toByteArray(value), 0, data, 30, 2);
     }
 
     public byte[] getData() {
