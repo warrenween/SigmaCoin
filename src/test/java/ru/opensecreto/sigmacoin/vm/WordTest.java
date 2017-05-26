26af7a98ac725aad502b335cd8ce2205b99b0644
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-26 12:14:31 +0300

    testing sum

--- src/test/java/ru/opensecreto/sigmacoin/vm/WordTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/vm/WordTest.java
@@ -1,8 +1,10 @@
 package ru.opensecreto.sigmacoin.vm;
 
-import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 
+import javax.xml.bind.DatatypeConverter;
+
+import static javax.xml.bind.DatatypeConverter.parseHexBinary;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WordTest {
@@ -13,6 +15,14 @@ public class WordTest {
         assertThat(new Word(4).equals("a")).isFalse();
         assertThat(new Word(4).equals(new Word(3))).isFalse();
         assertThat(new Word(4).equals(new Word(4))).isTrue();
+    }
+
+    @Test
+    public void testSum() {
+        Word a = new Word(parseHexBinary("d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"));
+        Word b = new Word(parseHexBinary("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"));
+        Word result = a.sum(b);
+        assertThat(result.equals(new Word(parseHexBinary("b824227cbf5a33037b3b4e3a250b1627294a49bd1b60b2dd7ecfff8a6466638a"))))
     }
 
 }
