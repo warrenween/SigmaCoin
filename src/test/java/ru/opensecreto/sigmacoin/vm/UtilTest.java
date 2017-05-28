f4c794a29c2ecf06692530b788543c5696b167b4
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-28 13:39:19 +0300

    better readability

--- src/test/java/ru/opensecreto/sigmacoin/vm/UtilTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/vm/UtilTest.java
@@ -9,29 +9,29 @@ public class UtilTest {
     public void testShiftLeft() {
         byte[] data1 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data1, 2);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
 
         byte[] data2 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data2, 8);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0001, 0b0000_0000});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0001, 0b0000_0000});
 
         byte[] data3 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data3, 9);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0010, 0b0000_0000});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0010, 0b0000_0000});
     }
 
     @Test
     public void testShiftRight() {
         byte[] data1 = new byte[]{0b0001_0000, 0b0001_0000};
         Util.shiftRight(data1, 2);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
 
         byte[] data2 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data2, 8);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0000, 0b0000_0001});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0000, 0b0000_0001});
 
         byte[] data3 = new byte[]{0b0001_0000, 0b0001_0000};
         Util.shiftLeft(data3, 9);
-        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0000, 0b0000_1000});
+        Assertions.assertThat(data1).inBinary().containsExactly(new byte[]{0b0000_0000, 0b0000_1000});
     }
 }
