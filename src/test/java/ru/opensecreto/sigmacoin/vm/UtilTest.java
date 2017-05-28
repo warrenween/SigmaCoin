a0ce9144c7f1234e9f4d09aeba37b220bc67aa8d
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-28 13:38:52 +0300

    shift right

--- src/test/java/ru/opensecreto/sigmacoin/vm/UtilTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/vm/UtilTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class UtilTest {
 
     @Test
-    public void testShift() {
+    public void testShiftLeft() {
         byte[] data1 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data1, 2);
         Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
@@ -18,5 +18,20 @@ public class UtilTest {
         byte[] data3 = new byte[]{0b0000_0001, 0b0000_0001};
         Util.shiftLeft(data3, 9);
         Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0010, 0b0000_0000});
+    }
+
+    @Test
+    public void testShiftRight() {
+        byte[] data1 = new byte[]{0b0001_0000, 0b0001_0000};
+        Util.shiftRight(data1, 2);
+        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0100, 0b0000_0100});
+
+        byte[] data2 = new byte[]{0b0000_0001, 0b0000_0001};
+        Util.shiftLeft(data2, 8);
+        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0000, 0b0000_0001});
+
+        byte[] data3 = new byte[]{0b0001_0000, 0b0001_0000};
+        Util.shiftLeft(data3, 9);
+        Assertions.assertThat(data1).containsExactly(new byte[]{0b0000_0000, 0b0000_1000});
     }
 }
