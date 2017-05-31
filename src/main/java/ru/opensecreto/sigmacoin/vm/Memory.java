79d93bee3aa105b3b1f85f316cdfcbe9fc0add00
Mr_Ramych <qwertyppp11@gmail.com>
2017-05-31 10:52:51 +0300

    improved memory class

--- src/main/java/ru/opensecreto/sigmacoin/vm/Memory.java
+++ src/main/java/ru/opensecreto/sigmacoin/vm/Memory.java
@@ -2,8 +2,29 @@ package ru.opensecreto.sigmacoin.vm;
 
 public interface Memory {
 
+    /**
+     * Get word at specified position. If word has not been set 0x00 is returned by default.
+     *
+     * @param pointer position of word to get
+     * @return word with specified pointer or 0x00 if word is unset
+     */
     public Word get(long pointer);
 
-    public Word put(long pointer, byte data);
+    /**
+     * Empty word at specified position ie. set 0x00 at specified poistion
+     *
+     * @param pointer position of word to empty
+     */
+    public default void remove(long pointer) {
+        set(pointer, Word.WORD_0);
+    }
+
+    /**
+     * Set given value at specified position
+     *
+     * @param pointer position of word to set
+     * @param word    word to set at given position
+     */
+    public void set(long pointer, Word word);
 
 }
