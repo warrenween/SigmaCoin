afa0cd338ef5b9741ebc79e71c9336ab965a6293
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-02 20:55:00 +0300

    reworked miner

---
+++ src/main/java/ru/opensecreto/sigmacoin/por/work/Miner.java
@@ -0,0 +1,200 @@
+package ru.opensecreto.sigmacoin.por.work;
+
+import com.google.common.primitives.Ints;
+import org.bouncycastle.crypto.Digest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.opensecreto.sigmacoin.core.DigestProvider;
+
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class Miner implements Callable<int[]> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Miner.class);
+
+    private final int maxChunkCount;
+    private final byte[] data;
+    private final DigestProvider digestProvider;
+    private final int n;
+    private final byte[] target;
+
+    public Miner(byte[] data, DigestProvider digestProvider, int n, byte[] target, int maxChunkCount) {
+        if (data == null) throw new IllegalArgumentException("data can not be null");
+        if (digestProvider == null) throw new IllegalArgumentException("Digest provider can not be null.");
+        if (target == null) throw new IllegalArgumentException("target can not be null");
+
+        if (digestProvider.getDigest() == null)
+            throw new IllegalArgumentException("Digest provider can not return null");
+        if (n < 2) throw new IllegalArgumentException("N must be >= 2");
+        if (digestProvider.getDigest().getDigestSize() != target.length)
+            throw new IllegalArgumentException("Target bytes count is not equal to digest size.");
+        if (maxChunkCount < n) throw new IllegalArgumentException("Max chunk count can not be less than N.");
+
+        this.data = Arrays.copyOf(data, data.length);
+        this.digestProvider = digestProvider;
+        this.n = n;
+        this.target = Arrays.copyOf(target, target.length);
+        this.maxChunkCount = maxChunkCount;
+    }
+
+    @Override
+    public int[] call() throws Exception {
+        //counters
+        int digestLength = digestProvider.getDigest().getDigestSize();
+        long stepCounter = 0;
+        BigInteger attemptsDone = BigInteger.ZERO;
+        int chunkCounter = Integer.MIN_VALUE;
+        long chunksGenerated = 0;
+        //values are negative to cover 100% of integer values
+        //ints are signed so if we start with 0 we will cover 1/2 ov all possible values
+
+        int chunkMinId = Integer.MIN_VALUE;
+        int chunkMaxId = Integer.MIN_VALUE + maxChunkCount - 1;
+
+        //store ids for selecting chunks from chunks array
+        //modify this values to select new chunks set
+        int[] ids = new int[n];
+        //store chunks
+        byte[][] chunks = new byte[maxChunkCount][digestLength];
+        //map each chunk to its id
+        int[] chunkId = new int[maxChunkCount];
+        int[] finalIds = new int[n];
+
+        //needed stuff
+        Digest digest = digestProvider.getDigest();
+        byte[] result = new byte[digestLength];
+
+        LOGGER.info("Successfully prepared. Starting mining.");
+
+        //checking chunkMaxId has already overflew
+        while (chunkMaxId > chunkMinId) {
+            LOGGER.debug("Started mining step {}.", stepCounter);
+
+            //generating chunks
+            long chunkTimeNanoStart = System.nanoTime();
+
+            for (int i = 0; i < maxChunkCount; i++) {
+                chunksGenerated++;
+                digest.reset();
+
+                digest.update(Ints.toByteArray(chunkCounter), 0, Integer.BYTES);
+                digest.update(data, 0, data.length);
+
+                digest.doFinal(chunks[i], 0);
+                chunkId[i] = chunkCounter;
+                chunkCounter++;
+            }
+
+            long chunkTimeNanoElapsed = System.nanoTime() - chunkTimeNanoStart;
+            LOGGER.trace("Generated {} chunks. Total {}sec. Time per one {}ns.",
+                    maxChunkCount, TimeUnit.NANOSECONDS.toSeconds(chunkTimeNanoElapsed),
+                    chunkTimeNanoElapsed / maxChunkCount);
+
+            for (int i = 0; i < ids.length; i++) {
+                ids[i] = i;
+            }
+
+            //solving
+            long solvingTimeNanoStart = System.nanoTime();
+
+            for (int i = ids.length - 1; i >= 0; i--) {
+                boolean mine = true;
+
+                while (mine) {
+                    //resetting result cache
+                    for (int j = 0; j < result.length; j++) {
+                        result[j] = 0;
+                    }
+
+                    //xoring chunks
+                    System.arraycopy(chunks[ids[0]], 0, result, 0, digestLength);
+                    for (int j = 1; j < ids.length; j++) {
+                        for (int k = 0; k < digestLength; k++) {
+                            result[k] ^= chunks[ids[j]][k];
+                        }
+                    }
+
+                    //checking solution
+                    boolean isValid = true;
+                    for (int j = 0; j < result.length; j++) {
+                        isValid &= result[j] == 0;
+                    }
+                    attemptsDone = attemptsDone.add(BigInteger.ONE);
+
+                    //checking final
+                    boolean solutionFound = false;
+                    if (isValid) {
+                        //sorting unsigned
+                        long[] tmp = new long[n];
+                        for (int j = 0; j < n; j++) {
+                            tmp[j] = ids[j] & 0xffffffffL;
+                        }
+                        Arrays.sort(tmp);
+                        int[] tmpInt = new int[n];
+                        for (int j = 0; j < n; j++) {
+                            tmpInt[j] = (int) tmp[j];
+                        }
+
+                        digest.reset();
+                        digest.update(data, 0, data.length);
+                        for (int j = 0; j < finalIds.length; j++) {
+                            digest.update(Ints.toByteArray(j), 0, Integer.BYTES);
+                        }
+                        digest.doFinal(result, 0);
+
+                        //checking if hash is less than header
+                        boolean search = true;
+                        int j = 0;
+                        while ((j < result.length) & search) {
+                            if ((result[j] & 0xff) > (target[j] & 0xff)) {
+                                search = false;
+                                solutionFound = false;
+                            }
+                            if ((result[j] & 0xff) < (target[i] & 0xff)) {
+                                search = false;
+                                solutionFound = true;
+                            }
+
+                            j++;
+                        }
+                    }
+
+                    if (solutionFound) {
+                        long solvingNanoElapsed = System.nanoTime() - solvingTimeNanoStart;
+                        LOGGER.info("Found valid solution. Attempts done {}. Chunks generated {}. " +
+                                        "Total time {}. Time per solution {}ns.",
+                                attemptsDone, chunksGenerated, attemptsDone, Duration.ofNanos(solvingNanoElapsed),
+                                BigInteger.valueOf(solvingNanoElapsed).divide(attemptsDone));
+                    }
+
+                    //updating ids for next solution
+                    if (i == (n - 1)) {
+                        if (ids[i] == (maxChunkCount - 1)) {
+                            mine = false;
+                        } else {
+                            ids[i]++;
+                        }
+                    } else {
+                        if (ids[i] == (ids[i + 1] - 1)) {
+                            mine = false;
+                        } else {
+                            ids[i]++;
+                        }
+                    }
+
+                }
+            }
+
+            LOGGER.debug("Step {} was not lucky. Chunks processed {}. Attempts done {}",
+                    stepCounter, chunksGenerated += maxChunkCount, attemptsDone);
+            chunksGenerated += maxChunkCount;
+            stepCounter++;
+        }
+        LOGGER.info("Unable to find solution.");
+        throw new Exception("Unable to compute result.");
+    }
+}
---
+++ src/test/java/ru/opensecreto/sigmacoin/por/pow/MinerTest.java
@@ -0,0 +1,59 @@
+package ru.opensecreto.sigmacoin.por.pow;
+
+import com.google.common.primitives.Ints;
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.digests.SHA3Digest;
+import org.bouncycastle.crypto.digests.ShortenedDigest;
+import org.testng.annotations.Test;
+import ru.opensecreto.sigmacoin.core.DigestProvider;
+import ru.opensecreto.sigmacoin.por.work.Miner;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MinerTest {
+
+    @Test
+    public void test() throws Exception {
+        DigestProvider provider = () -> new ShortenedDigest(new SHA3Digest(256), 1);
+
+        byte[] data = new byte[16];
+        new Random().nextBytes(data);
+        byte[] dataCopy = Arrays.copyOf(data, 16);
+
+        byte[] target = new byte[]{0x0f};
+        byte[] targetCopy = new byte[]{0x0f};
+
+        Miner miner = new Miner(data, provider, 8, target, 10000);
+        int[] result = miner.call();
+
+        //assert original arrays are unchanged
+        assertThat(data).containsExactly(dataCopy);
+        assertThat(target).containsExactly(targetCopy);
+
+        //checking valid data (not hash)
+        assertThat(result).hasSize(8);
+        //checking ordered. Ids are unsigned
+        long[] tmp = new long[8];
+        for (int i = 0; i < result.length; i++) {
+            tmp[i] = result[i] & 0xffffffffL;
+        }
+        long previous = tmp[0];
+        for (int i = 1; i < tmp.length; i++) {
+            assertThat(tmp[i]).overridingErrorMessage("Array is expected to be ordered.").isGreaterThan(previous);
+        }
+
+        Digest digest = provider.getDigest();
+        digest.update(data, 0, data.length);
+        for (int i = 0; i < result.length; i++) {
+            digest.update(Ints.toByteArray(result[i]), 0, Integer.BYTES);
+        }
+        byte[] hash = new byte[1];
+        digest.doFinal(hash, 0);
+        assertThat((hash[0] & 0xff) < 0x0f);
+
+    }
+
+}
