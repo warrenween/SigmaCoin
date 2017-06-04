37d51f6d04f7ca309d9ef218d8ec0ac0efc79042
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 13:16:05 +0300

    21

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
-    chef oraclejdk8
+    chef-solo oraclejdk8
     #sudo apt-get -y --force-yes install oracle-java8-installer
     #./jdk_switcher.sh use oraclejdk8
     java -version
@@ -10,7 +10,7 @@ if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
 fi
 
 if [ "${BUILD_TYPE}" = "OPEN_JDK_8" ]; then
-    chef openjdk8
+    chef-solo openjdk8
     #sudo apt-get install openjdk-8-jdk
     #./jdk_switcher.sh use openjdk8
     java -version
