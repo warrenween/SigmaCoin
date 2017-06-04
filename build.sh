99740c77d8b1e4895eb391b63507ac3a5f751bf0
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 13:10:02 +0300

    20

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -ev
 if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
-    sudo apt-get -y --force-yes install oracle-java8-installer
-    ./jdk_switcher.sh use oraclejdk8
+    chef oraclejdk8
+    #sudo apt-get -y --force-yes install oracle-java8-installer
+    #./jdk_switcher.sh use oraclejdk8
     java -version
     javac -version
     ./gradlew test
 fi
 
 if [ "${BUILD_TYPE}" = "OPEN_JDK_8" ]; then
-    sudo apt-get install openjdk-8-jdk
-    ./jdk_switcher.sh use openjdk8
+    chef openjdk8
+    #sudo apt-get install openjdk-8-jdk
+    #./jdk_switcher.sh use openjdk8
     java -version
     javac -version
     ./gradlew test
