e52f7226246ed6a2065270adffe7edfff6572d21
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 12:56:32 +0300

    18

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
+    sudo apt-get -y --force-yes install oracle-java8-installer
     ./jdk_switcher.sh use oraclejdk8
     java -version
     javac -version
