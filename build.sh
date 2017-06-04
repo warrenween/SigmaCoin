8e7f1ac5a53d66f77986946bca6d22a9877ce2fd
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 12:36:02 +0300

    12

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
-if [ "${BUILD_TYPE}" = "JDK8" ]; then
+if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
     sudo jdk_switcher use oraclejdk8
     ./gradlew test
 fi
 
-if [ "${BUILD_TYPE}" = "OPENJDK8" ]; then
+if [ "${BUILD_TYPE}" = "OPEN_JDK_8" ]; then
     sudo jdk_switcher use openjdk8
     ./gradlew test
 fi
