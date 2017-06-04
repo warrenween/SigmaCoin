265b27032233378d2228a4e06f43c4e4dd549c80
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 13:42:51 +0300

    30

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
 if [ "${BUILD_TYPE}" = "ORACLE_JDK_8" ]; then
-    sudo add-apt-repository  ppa:webupd8team/java -y
+    sudo add-apt-repository ppa:webupd8team/java -y
     sudo apt-get update
     sudo apt-get install oracle-java8-installer
     sudo apt install oracle-java8-set-default
-    #./jdk_switcher.sh use oraclejdk8
+    ./jdk_switcher.sh use oraclejdk8
     java -version
     javac -version
     ./gradlew test
@@ -14,7 +14,7 @@ fi
 if [ "${BUILD_TYPE}" = "OPEN_JDK_8" ]; then
     sudo add-apt-repository ppa:openjdk-r/ppa -y
     sudo apt-get update
-    sudo apt-get install openjdk-8
+    sudo apt-get install openjdk-8-jdk
     #./jdk_switcher.sh use openjdk8
     java -version
     javac -version
