75b187d2eb3c61c76afddeb45cd474d009ac1188
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 12:34:25 +0300

    11

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -ev
 if [ "${BUILD_TYPE}" = "JDK8" ]; then
-    sudo apt-get -y --force-yes install oracle-java8-installer
-    sudo apt-get -y --force-yes install oracle-java8-set-default
+    sudo jdk_switcher use oraclejdk8
     ./gradlew test
 fi
 
 if [ "${BUILD_TYPE}" = "OPENJDK8" ]; then
-    sudo apt-get -y --force-yes install openjdk-8-jdk
+    sudo jdk_switcher use openjdk8
     ./gradlew test
 fi
