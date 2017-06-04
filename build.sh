b7ab1dc85ac1bde472e1f900b355547cbc5feb7e
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-04 11:36:41 +0300

    simple script

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
 set -ev
+
+if [ "${BUILD_TYPE}" = "JDK8" ]; then
+    sudo apt-get -y --force-yes install oracle-java8-installer
+    ./gradlew test
+fi
+
+if [ "${BUILD_TYPE}" = "OPENJDK8" ]; then
+    sudo apt-get -y --force-yes install java-1.8.0-openjdk
+    ./gradlew test
+fi
