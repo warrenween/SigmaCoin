5d984506f53f99011ac3d476e085fe208a4b1778
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-05 13:32:02 +0300

    transaction CuLimit and CuPrice

--- src/main/java/ru/opensecreto/sigmacoin/blockchain/Transaction.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockchain/Transaction.java
@@ -9,16 +9,22 @@ public class Transaction {
     public final static int CHAINID_SIZE = 4;
     public final static int SOURCE_SIZE = 32;
     public final static int TARGET_SIZE = 32;
+    public final static int CULIMIT_SIZE = 16;
+    public final static int CUPRICE_SIZE = 32;
 
     private final byte[] chainID;
     private final long timestamp;
     private final byte[] source;
     private final byte[] target;
+    private final byte[] cuLimit;
+    private final byte[] cuPrice;
     private final byte[] data;
     private final short sigmethod;
     private final byte[] signature;
 
-    public Transaction(byte[] chainID, long timestamp, byte[] source, byte[] target, byte[] data, short sigmethod, byte[] signature)
+    public Transaction(byte[] chainID, long timestamp, byte[] source, byte[] target,
+                       byte[] cuLimit, byte[] cuPrice,
+                       byte[] data, short sigmethod, byte[] signature)
             throws IllegalArgumentException {
         //chainid verification
         if (chainID == null) throw new IllegalArgumentException("Chain id can not be null");
@@ -32,6 +38,14 @@ public class Transaction {
         //target verification
         if (target == null) throw new IllegalArgumentException("Target is null.");
         if (target.length != TARGET_SIZE) throw new IllegalArgumentException("Invalid target size");
+
+        //cuLimit verification
+        if (cuLimit == null) throw new IllegalArgumentException("CuLimit is null");
+        if (cuLimit.length != CULIMIT_SIZE) throw new IllegalArgumentException("CuLimit hash incorrect length");
+
+        //cuPrice verification
+        if (cuPrice == null) throw new IllegalArgumentException("CuPrice is null");
+        if (cuPrice.length != CULIMIT_SIZE) throw new IllegalArgumentException("CuPrice hash incorrect length");
 
         //sigmethod verification
         if (!Signers.SIGNERS.containsKey(sigmethod))
@@ -48,6 +62,8 @@ public class Transaction {
         this.timestamp = timestamp;
         this.source = Arrays.copyOf(source, SOURCE_SIZE);
         this.target = Arrays.copyOf(target, TARGET_SIZE);
+        this.cuLimit = Arrays.copyOf(cuLimit, CULIMIT_SIZE);
+        this.cuPrice = Arrays.copyOf(cuPrice, CUPRICE_SIZE);
         this.data = Arrays.copyOf(data, data.length);
         this.sigmethod = sigmethod;
         this.signature = Arrays.copyOf(signature, signature.length);
