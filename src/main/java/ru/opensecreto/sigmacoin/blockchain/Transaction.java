94f8415bbc944a33a97bb2b55580f0066c5f290f
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-07 11:37:02 +0300

    simplified argument checks and removed cuLimit and cuPrice filed length limit

--- src/main/java/ru/opensecreto/sigmacoin/blockchain/Transaction.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockchain/Transaction.java
@@ -4,13 +4,14 @@ import ru.opensecreto.sigmacoin.crypto.Signers;
 
 import java.util.Arrays;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class Transaction {
 
     public final static int CHAINID_SIZE = 4;
     public final static int SOURCE_SIZE = 32;
     public final static int TARGET_SIZE = 32;
-    public final static int CULIMIT_SIZE = 16;
-    public final static int CUPRICE_SIZE = 32;
 
     private final byte[] chainID;
     private final long timestamp;
@@ -18,54 +19,41 @@ public class Transaction {
     private final byte[] target;
     private final byte[] cuLimit;
     private final byte[] cuPrice;
-    private final byte[] data;
     private final short sigmethod;
     private final byte[] signature;
+    private final byte[] data;
+
 
     public Transaction(byte[] chainID, long timestamp, byte[] source, byte[] target,
-                       byte[] cuLimit, byte[] cuPrice,
-                       byte[] data, short sigmethod, byte[] signature)
-            throws IllegalArgumentException {
+                       byte[] cuLimit, byte[] cuPrice, short sigmethod, byte[] signature,
+                       byte[] data)
+            throws IllegalArgumentException, NullPointerException {
         //chainid verification
-        if (chainID == null) throw new IllegalArgumentException("Chain id can not be null");
-        if (chainID.length != CHAINID_SIZE) throw new IllegalArgumentException("Invalid chainId field length");
+        checkNotNull(chainID);
+        checkNotNull(source);
+        checkNotNull(target);
+        checkNotNull(cuLimit);
+        checkNotNull(cuPrice);
+        checkNotNull(signature);
+        checkNotNull(data);
 
-        //source verification
-        if (source == null) throw new IllegalArgumentException("Source can not be null");
-        if (source.length != SOURCE_SIZE)
-            throw new IllegalArgumentException("Invalid source field length.");
-
-        //target verification
-        if (target == null) throw new IllegalArgumentException("Target is null.");
-        if (target.length != TARGET_SIZE) throw new IllegalArgumentException("Invalid target size");
-
-        //cuLimit verification
-        if (cuLimit == null) throw new IllegalArgumentException("CuLimit is null");
-        if (cuLimit.length != CULIMIT_SIZE) throw new IllegalArgumentException("CuLimit hash incorrect length");
-
-        //cuPrice verification
-        if (cuPrice == null) throw new IllegalArgumentException("CuPrice is null");
-        if (cuPrice.length != CULIMIT_SIZE) throw new IllegalArgumentException("CuPrice hash incorrect length");
-
-        //sigmethod verification
-        if (!Signers.SIGNERS.containsKey(sigmethod))
-            throw new IllegalArgumentException("Unknown signature method " + sigmethod);
-
-        //signature verification
-        if (signature == null) throw new IllegalArgumentException("Signature is null");
-        if (signature.length != Signers.SIG_SIZES.get(sigmethod))
-            throw new IllegalArgumentException("Signature of type " + Signers.SIGNERS_NAMES.get(sigmethod) +
-                    " expected to have length of " + Signers.SIG_SIZES.get(sigmethod));
-
+        checkArgument(chainID.length != CHAINID_SIZE, "Invalid chainId field length");
+        checkArgument(source.length != SOURCE_SIZE, "Invalid source field length.");
+        checkArgument(target.length != TARGET_SIZE, "Invalid target size");
+        checkArgument(Signers.SIGNERS.containsKey(sigmethod), "Unknown signature method " + sigmethod);
+        checkArgument(signature.length != Signers.SIG_SIZES.get(sigmethod),
+                "Signature of type " + Signers.SIGNERS_NAMES.get(sigmethod) +
+                        " expected to have length of " + Signers.SIG_SIZES.get(sigmethod));
 
         this.chainID = Arrays.copyOf(chainID, CHAINID_SIZE);
         this.timestamp = timestamp;
         this.source = Arrays.copyOf(source, SOURCE_SIZE);
         this.target = Arrays.copyOf(target, TARGET_SIZE);
-        this.cuLimit = Arrays.copyOf(cuLimit, CULIMIT_SIZE);
-        this.cuPrice = Arrays.copyOf(cuPrice, CUPRICE_SIZE);
-        this.data = Arrays.copyOf(data, data.length);
+        this.cuLimit = Arrays.copyOf(cuLimit, cuLimit.length);
+        this.cuPrice = Arrays.copyOf(cuPrice, cuPrice.length);
         this.sigmethod = sigmethod;
         this.signature = Arrays.copyOf(signature, signature.length);
+        this.data = Arrays.copyOf(data, data.length);
     }
+
 }
