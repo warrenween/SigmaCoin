e1c358fd9ed6e8b04778d753988418bee41bd0c7
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-10 18:04:03 +0300

    removed unused configs from Signers class

--- src/main/java/ru/opensecreto/sigmacoin/crypto/Signers.java
+++ src/main/java/ru/opensecreto/sigmacoin/crypto/Signers.java
@@ -10,18 +10,6 @@ import java.util.Map;
 
 public class Signers {
 
-    public static final Map<BigInteger, BaseSigner> SIGNERS = Collections.unmodifiableMap(new HashMap<BigInteger, BaseSigner>() {{
-        put(BigInteger.valueOf(1), new Ed25519SHA512());
-    }});
-
-    public static final Map<BigInteger, String> SIGNERS_NAMES = Collections.unmodifiableMap(new HashMap<BigInteger, String>() {{
-        put(BigInteger.valueOf(1), "Ed25519-SHA512");
-    }});
-
-    public static final Map<BigInteger, Integer> SIG_SIZES = Collections.unmodifiableMap(new HashMap<BigInteger, Integer>() {{
-        put(BigInteger.valueOf(1), 64);
-    }});
-
     public static final Map<Integer, SignerFactory> SIGNER_FACTORIES = Collections.unmodifiableMap(new HashMap<Integer, SignerFactory>() {{
         put(Ed25519SHA512.SIGNER_ID, Ed25519SHA512::new);
     }});
