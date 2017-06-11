3508840243c346f09154705012f578a7d2df22f1
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-11 22:34:56 +0300

    EmptySigner checks for null arguments + tests

--- src/main/java/ru/opensecreto/sigmacoin/crypto/empty/EmptySigner.java
+++ src/main/java/ru/opensecreto/sigmacoin/crypto/empty/EmptySigner.java
@@ -1,9 +1,11 @@
 package ru.opensecreto.sigmacoin.crypto.empty;
 
-import ru.opensecreto.sigmacoin.crypto.base.Signer;
 import ru.opensecreto.sigmacoin.crypto.base.PrivateKey;
 import ru.opensecreto.sigmacoin.crypto.base.PublicKey;
 import ru.opensecreto.sigmacoin.crypto.base.Signature;
+import ru.opensecreto.sigmacoin.crypto.base.Signer;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class EmptySigner implements Signer {
 
@@ -11,11 +13,16 @@ public class EmptySigner implements Signer {
 
     @Override
     public Signature sign(byte[] message, PrivateKey privateKey) {
+        checkNotNull(message);
+        checkNotNull(privateKey);
         return new Signature(0, new byte[0]);
     }
 
     @Override
     public boolean verify(byte[] message, Signature signature, PublicKey publicKey) {
+        checkNotNull(message);
+        checkNotNull(signature);
+        checkNotNull(publicKey);
         return true;
     }
 }
