c1a18abab94ce9a183110733570c877c7a868219
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-13 11:09:23 +0300

    BlockchainController can operate transactions

--- src/main/java/ru/opensecreto/sigmacoin/blockchaintorage/BlockchainStorageController.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockchaintorage/BlockchainStorageController.java
@@ -2,10 +2,14 @@ package ru.opensecreto.sigmacoin.blockchaintorage;
 
 import jetbrains.exodus.ArrayByteIterable;
 import jetbrains.exodus.env.*;
+import org.bouncycastle.crypto.Digest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.opensecreto.sigmacoin.blockchain.Transaction;
+import ru.opensecreto.sigmacoin.core.DigestProvider;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -18,9 +22,12 @@ public class BlockchainStorageController {
 
     private final Environment environment;
     private final Store blockStore;
+    private final Store transactionStore;
+    private final DigestProvider digestProvider;
 
-    public BlockchainStorageController(File dbDir, int hashSize) {
+    public BlockchainStorageController(File dbDir, int hashSize, DigestProvider digestProvider) {
         checkNotNull(dbDir);
+        this.digestProvider = checkNotNull(digestProvider);
 
         this.hashSize = hashSize;
 
@@ -28,6 +35,9 @@ public class BlockchainStorageController {
 
         blockStore = environment.computeInTransaction(
                 txn -> environment.openStore("blocks", StoreConfig.WITHOUT_DUPLICATES, txn, true)
+        );
+        transactionStore = environment.computeInTransaction(
+                txn -> environment.openStore("transactions", StoreConfig.WITHOUT_DUPLICATES, txn, true)
         );
     }
 
@@ -56,6 +66,39 @@ public class BlockchainStorageController {
             return blockStore.get(txn, new ArrayByteIterable(hash)).getBytesUnsafe();
         });
     }
+
+    public byte[] addTransaction(Transaction transaction)
+            throws NullPointerException {
+        checkNotNull(transaction);
+        final byte[] txData = Transaction.encode(transaction);
+        Digest digest = digestProvider.getDigest();
+        byte[] hash = new byte[digest.getDigestSize()];
+        digest.update(txData, 0, txData.length);
+        digest.doFinal(hash, 0);
+        environment.executeInTransaction(txn -> {
+            transactionStore.add(txn, new ArrayByteIterable(hash), new ArrayByteIterable(txData));
+        });
+        return Arrays.copyOf(hash, hash.length);
+    }
+
+    public boolean hasTransaction(byte[] hash)
+            throws NullPointerException, IllegalArgumentException {
+        checkNotNull(hash);
+        checkArgument(digestProvider.getDigestSize() == hash.length);
+        return environment.computeInReadonlyTransaction(txn ->
+                blockStore.get(txn, new ArrayByteIterable(hash)) != null
+        );
+    }
+
+    public Transaction getTransaction(byte[] hash) {
+        checkNotNull(hash);
+        checkArgument(hash.length == hashSize);
+        return Transaction.decode(environment.computeInReadonlyTransaction(txn -> {
+            if (!hasTransaction(hash)) throw new IllegalStateException("Can not find transaction.");
+            return transactionStore.get(txn, new ArrayByteIterable(hash)).getBytesUnsafe();
+        }));
+    }
+
 
     public void close() {
         environment.close();
--- src/test/java/ru/opensecreto/sigmacoin/blockchaintorage/BlockchainStorageControllerTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/blockchaintorage/BlockchainStorageControllerTest.java
@@ -1,8 +1,16 @@
 package ru.opensecreto.sigmacoin.blockchaintorage;
 
+import org.bouncycastle.crypto.Digest;
+import org.bouncycastle.crypto.digests.KeccakDigest;
+import org.bouncycastle.crypto.digests.SHA3Digest;
 import org.testng.annotations.Test;
+import ru.opensecreto.sigmacoin.blockchain.Transaction;
+import ru.opensecreto.sigmacoin.crypto.base.Signature;
+import ru.opensecreto.sigmacoin.vm.AccountAddress;
+import ru.opensecreto.sigmacoin.vm.Word;
 
 import java.io.File;
+import java.math.BigInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -12,7 +20,7 @@ public class BlockchainStorageControllerTest {
     @Test
     public void testInvalidArguments() {
         BlockchainStorageController controller = new BlockchainStorageController(
-                new File("testInvalid.db"), 4
+                new File("testInvalid.db"), 4, SHA3Digest::new
         );
 
         assertThatThrownBy(() -> controller.addBlock(new byte[3], new byte[4]))
@@ -25,9 +33,9 @@ public class BlockchainStorageControllerTest {
     }
 
     @Test
-    public void test() {
+    public void testBlocks() {
         BlockchainStorageController controller = new BlockchainStorageController(
-                new File("test.db"), 2
+                new File("test.db"), 2, SHA3Digest::new
         );
 
         assertThat(controller.hasBlock(new byte[]{1, 2})).isFalse();
@@ -48,7 +56,7 @@ public class BlockchainStorageControllerTest {
         //testing loading database
         controller.close();
         BlockchainStorageController controllerNew = new BlockchainStorageController(
-                new File("test.db"), 2
+                new File("test.db"), 2, SHA3Digest::new
         );
 
         //check reading block do not changes it
@@ -61,6 +69,45 @@ public class BlockchainStorageControllerTest {
         assertThat(controllerNew.addBlock(new byte[]{1, 2}, new byte[]{5, 6})).isFalse();
         assertThat(controllerNew.hasBlock(new byte[]{1, 2})).isTrue();
         assertThat(controllerNew.getBlock(new byte[]{1, 2})).containsExactly(new byte[]{3, 4});
+    }
+
+    @Test
+    public void testTransactions() {
+        BlockchainStorageController controller1 = new BlockchainStorageController(
+                new File("txTest"), 32, () -> new KeccakDigest(128)
+        );
+        Transaction transaction1 = new Transaction(
+                BigInteger.valueOf(1),
+                BigInteger.valueOf(2),
+                new AccountAddress(new Word(3)),
+                BigInteger.valueOf(4),
+                BigInteger.valueOf(5),
+                new Signature(1, new byte[5]),
+                new byte[20]
+        );
+
+        Digest digest = new KeccakDigest(128);
+        byte[] txData1 = Transaction.encode(transaction1);
+        digest.update(txData1, 0, txData1.length);
+        byte[] hash1 = new byte[digest.getDigestSize()];
+        digest.doFinal(hash1, 0);
+
+        assertThat(controller1.hasTransaction(hash1)).isFalse();
+        byte[] hashController1 = controller1.addTransaction(transaction1);
+        assertThat(controller1.hasTransaction(hash1)).isTrue();
+        assertThat(hashController1).containsExactly(hash1);
+
+        Transaction transactionController1 = controller1.getTransaction(hashController1);
+        assertThat(transaction1).isEqualTo(transactionController1);
+
+        controller1.close();
+        BlockchainStorageController controller2 = new BlockchainStorageController(
+                new File("txTest"), 32, () -> new KeccakDigest(128)
+        );
+        assertThat(controller2.hasTransaction(hash1)).isTrue();
+
+        Transaction transactionController2 = controller2.getTransaction(hashController1);
+        assertThat(transactionController2).isEqualTo(transaction1);
     }
 
 }
