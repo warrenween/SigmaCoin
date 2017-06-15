15dea8ac421657f5fea91791cae41bbff4642e5f
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-15 19:51:46 +0300

    encoding block header

--- src/main/java/ru/opensecreto/sigmacoin/blockchain/BlockHeader.java
+++ src/main/java/ru/opensecreto/sigmacoin/blockchain/BlockHeader.java
@@ -1,5 +1,11 @@
 package ru.opensecreto.sigmacoin.blockchain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
 
@@ -32,6 +38,39 @@ public class BlockHeader {
         checkArgument(timestamp.signum() >= 0);
         checkArgument(difficulty.signum() > 0);
         checkArgument(pow.length >= 2);
+    }
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(BlockHeader.class);
+
+    public static byte[] encode(BlockHeader blockHeader) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DataOutputStream dataOut = new DataOutputStream(out);
+        try {
+            byte[] heightData = blockHeader.height.toByteArray();
+            dataOut.writeInt(heightData.length);
+            out.write(heightData);
+
+            byte[] timestampData = blockHeader.timestamp.toByteArray();
+            dataOut.writeInt(timestampData.length);
+            out.write(timestampData);
+
+            byte[] difficultyData = blockHeader.difficulty.toByteArray();
+            dataOut.writeInt(difficultyData.length);
+            out.write(difficultyData);
+
+            out.write(blockHeader.parentHash);
+            out.write(blockHeader.txRootHash);
+            out.write(blockHeader.stateRootHash);
+
+            for (int i : blockHeader.pow) {
+                dataOut.writeInt(i);
+            }
+
+        } catch (IOException e) {
+            LOGGER.warn("Something unexpected happened when encoding block header.", e);
+            throw new RuntimeException(e);
+        }
+        return out.toByteArray();
     }
 
 }
