31122b9b9ebdafdc82ccb419b0eb3e88c6caa341
Mr_Ramych <qwertyppp11@gmail.com>
2017-06-15 22:25:32 +0300

    better testing BlockHeader.equals()

--- src/test/java/ru/opensecreto/sigmacoin/blockchain/BlockHeaderTest.java
+++ src/test/java/ru/opensecreto/sigmacoin/blockchain/BlockHeaderTest.java
@@ -1,11 +1,12 @@
 package ru.opensecreto.sigmacoin.blockchain;
 
-import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 import ru.opensecreto.sigmacoin.config.BaseConfig;
 
 import java.math.BigInteger;
 import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BlockHeaderTest {
 
@@ -34,7 +35,67 @@ public class BlockHeaderTest {
                 a1, a2, a3,
                 pow
         );
-        Assertions.assertThat(header1.equals(header2)).isTrue();
+        assertThat(header1.equals(header2)).isTrue();
+    }
+
+    @Test
+    public void testEqualsFalse() {
+        byte[] a1 = new byte[BaseConfig.DEFAULT_IDENTIFICATOR_LENGTH];
+        byte[] a2 = new byte[BaseConfig.DEFAULT_IDENTIFICATOR_LENGTH];
+        byte[] a3 = new byte[BaseConfig.DEFAULT_IDENTIFICATOR_LENGTH];
+        int[] pow = new int[4];
+
+        Random rnd = new Random();
+        rnd.nextBytes(a1);
+        rnd.nextBytes(a2);
+        rnd.nextBytes(a3);
+        for (int i = 0; i < pow.length; i++) {
+            pow[i] = rnd.nextInt();
+        }
+
+        BlockHeader header = new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                a1, a2, a3,
+                pow
+        );
+
+        assertThat(header.equals(null)).isFalse();
+        assertThat(header.equals("")).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(100), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                a1, a2, a3,
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(200), BigInteger.valueOf(3),
+                a1, a2, a3,
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(300),
+                a1, a2, a3,
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                new byte[32], a2, a3,
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                a1, new byte[32], a3,
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                a1, a2, new byte[32],
+                pow
+        ))).isFalse();
+        assertThat(header.equals(new BlockHeader(
+                BigInteger.valueOf(1), BigInteger.valueOf(2), BigInteger.valueOf(3),
+                a1, a2, a3,
+                new int[4]
+        ))).isFalse();
     }
 
 }
